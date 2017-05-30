bbe764c644325cc62ce2212b5808f350bf7109ba
Jacob Panikulam <jpanikulam@ufl.edu>
2017-05-29 22:35:54 -0400

    Working normalization and start of ncc

--- vision/features.hh
+++ vision/features.hh
@@ -1,1 +1,11 @@
 #pragma once
+
+#include <opencv2/opencv.hpp>
+
+namespace slam {
+cv::Mat convolve(const cv::Mat &image, const cv::Mat &kernel);
+
+cv::Mat ncc(const cv::Mat &image, const cv::Mat &kernel);
+
+float compute_variance(const cv::Mat &image);
+}
--- vision/main.cc
+++ vision/main.cc
@@ -1,17 +1,20 @@
-#include <iostream>
-
-#include <Eigen/Dense>
-#include <opencv2/opencv.hpp>
-
 #include "features.hh"
 #include "images.hh"
 #include "io.hh"
 #include "pyramid.hh"
+#include "histogram_equalize.hh"
+#include "block_transform.hh"
+
+#include "util/clamp.hh"
+
+#include <iostream>
+#include <Eigen/Dense>
+#include <opencv2/opencv.hpp>
 
 using Vec2 = Eigen::Vector2f;
 
 struct MappingContext {
-  cv::Mat           rgb_image;
+  cv::Mat rgb_image;
   std::vector<Vec2> points;
 };
 
@@ -19,7 +22,7 @@ void visualize(const MappingContext &context) {
   cv::Mat visualization_image = context.rgb_image;
 
   for (const auto &point : context.points) {
-    constexpr int    RADIUS = 1;
+    constexpr int RADIUS = 1;
     const cv::Scalar COLOR(0, 255, 0);
     cv::circle(visualization_image, cv::Point(point.x(), point.y()), RADIUS, COLOR);
   }
@@ -39,59 +42,15 @@ cv::Mat normalize(const cv::Mat &grayscale_image) {
   return (float_gs_image - min) / (max - min);
 }
 
-float variance(const cv::Mat &image) {
-  float  mean   = cv::mean(image)[0];
-  double sum_sq = 0.0;
-
-  for (int col = 0; col < image.cols; ++col) {
-    for (int row = 0; row < image.rows; ++row) {
-      const double pixel_val = image.at<float>(row, col);
-      sum_sq += (pixel_val - mean) * (pixel_val - mean);
-    }
-  }
-
-  const double num_elements = image.total();
-  const double variance     = sum_sq / num_elements;
-  return variance;
-}
-
-cv::Mat block_normalize(const cv::Mat &input_image) {
-  //
-
-  // OpenCV API sucks
-  cv::Mat output = input_image.clone();
-
-  // std::cout << slam::type2str(output.type()) << std::endl;
-
-  constexpr int BLOCK_SIZE = 100;
-  for (int start_col = 0; (start_col + BLOCK_SIZE) < output.cols; start_col += BLOCK_SIZE) {
-    for (int start_row = 0; (start_row + BLOCK_SIZE) < output.rows; start_row += BLOCK_SIZE) {
-      const auto    rect      = cv::Rect(start_col, start_row, BLOCK_SIZE, BLOCK_SIZE);
-      const cv::Mat sub_image = output(rect);
-
-      const float  var    = variance(sub_image);
-      const float  stddev = var * var;
-      const double mean   = cv::mean(sub_image)[0];
-
-      std::cout << stddev << ", " << mean << std::endl;
-
-      output(rect) = normalize((sub_image - mean) / stddev);
-    }
-  }
-  cv::imshow("Merk", output);
-  cv::waitKey(0);
-  return output;
-}
-
 std::vector<Vec2> extract_harris_features(const cv::Mat &image) {
-  constexpr double RELATIVE_SCALE     = 1.0;
+  constexpr double RELATIVE_SCALE = 1.0;
   constexpr double INV_RELATIVE_SCALE = 1.0 / RELATIVE_SCALE;
 
   cv::Mat scaled_image;
   cv::resize(image, scaled_image, cv::Size(0, 0), RELATIVE_SCALE, RELATIVE_SCALE);
-  const cv::Mat normalized      = normalize(scaled_image);
-  const float   mean            = cv::mean(normalized)[0];
-  cv::Mat       mean_subtracted = normalized - mean;
+  const cv::Mat normalized = normalize(scaled_image);
+  const float mean = cv::mean(normalized)[0];
+  cv::Mat mean_subtracted = normalized - mean;
 
   cv::imshow("Gombo", normalized - mean);
 
@@ -100,7 +59,7 @@ std::vector<Vec2> extract_harris_features(const cv::Mat &image) {
   std::vector<Vec2> locations;
   for (int col = 0; col < mean_subtracted.cols; ++col) {
     for (int row = 0; row < mean_subtracted.rows; ++row) {
-      const int   index     = row * (mean_subtracted.cols) + col;
+      const int index = row * (mean_subtracted.cols) + col;
       const float pixel_val = pixel_ptr[index];
 
       constexpr float NORMALIZED_THRESHOLD = 0.05;
@@ -123,26 +82,40 @@ int main() {
     cv::cvtColor(image, gray_image, CV_BGR2GRAY);
     const cv::Mat_<float> normalized_gray_image = normalize(gray_image);
 
-    constexpr int    BLOCK_SIZE     = 2;
-    constexpr int    K_SIZE         = 5;
-    constexpr double FREE_PARAMETER = 0.001;
+    constexpr int BLOCK_TRANSFORM_SIZE = 100;
+    const cv::Mat equalized = slam::block_transform<BLOCK_TRANSFORM_SIZE>(gray_image, slam::histogram_equalize);
 
-    block_normalize(normalized_gray_image);
+    cv::imshow("Equalized", equalized);
 
-    cv::Mat harris_image;
-    cornerHarris(normalized_gray_image, harris_image, BLOCK_SIZE, K_SIZE, FREE_PARAMETER);
-    cv::Mat drawable_image = image;
+    const auto get_harris = [&](const cv::Mat &sub_img) {
+      constexpr int HARRIS_BLOCK_SIZE = 2;
+      constexpr int HARRIS_K_SIZE = 5;
+      constexpr double HARRIS_FREE_PARAMETER = 0.0001;
 
-    const auto pts = extract_harris_features(harris_image);
+      cv::Mat temp_harris_image;
+      cornerHarris(sub_img, temp_harris_image, HARRIS_BLOCK_SIZE, HARRIS_K_SIZE, HARRIS_FREE_PARAMETER);
+      return temp_harris_image;
+    };
 
-    MappingContext context;
-    context.points    = pts;
-    context.rgb_image = image;
-    visualize(context);
+    const cv::Mat harris_image = slam::block_transform<BLOCK_TRANSFORM_SIZE>(normalize(equalized), get_harris);
+
+    double min, max;
+    cv::minMaxLoc(harris_image, &min, &max);
+
+    std::cout << harris_image.size() << std::endl;
+    std::cout << "MIN: " << min << std::endl;
+    std::cout << "MAX: " << max << std::endl;
+    cv::imshow("Determinants", harris_image);
+    cv::imshow("Input", image);
+
+    // const auto pts = extract_harris_features(harris_image);
+
+    // MappingContext context;
+    // context.points = pts;
+    // context.rgb_image = image;
+    // visualize(context);
 
     const int key = cv::waitKey(0);
-    std::cout << key << std::endl;
-
     if (key == 113 || key == 1048689) {
       break;
     }
