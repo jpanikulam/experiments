e831f5982ec8a6ced7085b861f1bc5c854773c6f
Jacob Panikulam <jpanikul@gmail.com>
2017-06-12 22:53:02 -0400

    Add numerical jacobian

--- numerics/numdiff.hh
+++ numerics/numdiff.hh
@@ -4,7 +4,6 @@
 
 namespace numerics {
 
-// todo: move to numerics
 template <int cols, typename Callable>
 Eigen::Matrix<double, cols, 1> numerical_gradient(const Eigen::Matrix<double, cols, 1> &x,
                                                   const Callable &fcn,
@@ -18,6 +17,27 @@ Eigen::Matrix<double, cols, 1> numerical_gradient(const Eigen::Matrix<double, co
     zero(k)     = feps;
 
     jac(k) = (fcn(x + zero) - fcn(x - zero)) / (2 * feps);
+  }
+  return jac;
+}
+
+template <int output_rows, int input_rows, typename Callable>
+Eigen::Matrix<double, output_rows, input_rows> numerical_jacobian(const Eigen::Matrix<double, input_rows, 1> &x,
+                                                                  const Callable &fcn,
+                                                                  const double    feps = 1e-6) {
+  // df1/dx1    df1/dx2 ....
+  // df2/dx1    df2/dx2 ....
+  //    :          :
+  //    :          :
+
+  using OutVec   = Eigen::Matrix<double, input_rows, 1>;
+  using Jacobian = Eigen::Matrix<double, output_rows, input_rows>;
+  Jacobian jac   = Jacobian::Zero();
+
+  for (int k = 0; k < input_rows; ++k) {
+    OutVec zero = OutVec::Zero();
+    zero(k)     = feps;
+    jac.col(k)  = (fcn(x + zero) - fcn(x - zero)) / (2 * feps);
   }
   return jac;
 }
--- numerics/numdiff_test.cc
+++ numerics/numdiff_test.cc
@@ -1,5 +1,7 @@
 #include "numdiff.hh"
 
+#include "eigen.hh"
+
 #include <gtest/gtest.h>
 
 using Vec1 = Eigen::Matrix<double, 1, 1>;
@@ -9,6 +11,18 @@ namespace numerics {
 namespace {
 double gfunc(const Vec2 &z) {
   return z(0) * z(0) + 5 * z(1);
+}
+
+Vec2 zfunc(const Vec3 &a) {
+  return Vec2(a(0) * a(1), 4.0 * a(2));
+}
+
+MatNd<2, 3> dzfunc_da(const Vec3 &a) {
+  MatNd<2, 3> J;
+
+  J.row(0) << a(1), a(0), 0.0;
+  J.row(1) << 0.0, 0.0, 4.0;
+  return J;
 }
 }
 
@@ -34,6 +48,21 @@ TEST(NumericalGradient, gfunc) {
 
   // max error
   EXPECT_LT((J - expected).lpNorm<Eigen::Infinity>(), EPS);
+}
+
+TEST(NumericalJacobian, zfunc) {
+  // const double EPS = 1e-6;
+
+  const std::vector<Vec3> test_pts = {Vec3(0.0, 0.0, 0.0), Vec3(1.0, -2.0, 7.0), Vec3(9.0, -12.0, 0.0),
+                                      Vec3(1.0, 2.0, 1.0)};
+
+  for (const auto x : test_pts) {
+    const MatNd<2, 3> J          = numerical_jacobian<2, 3>(x, zfunc);
+    const MatNd<2, 3> expected_J = dzfunc_da(x);
+
+    constexpr double EPS = 1e-3;
+    EXPECT_LT((J - expected_J).norm(), EPS);
+  }
 }
 }
 
