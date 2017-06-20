4ec9f0e7b0740ad633fd4a752cddcc1ba056b69c
Jacob Panikulam <jpanikul@gmail.com>
2017-06-19 23:26:30 -0400

    Add helpful sophus types header

---
+++ sophus.hh
@@ -0,0 +1,10 @@
+#include <sophus/se2.hpp>
+#include <sophus/se3.hpp>
+#include <sophus/sim3.hpp>
+
+using SO3  = Sophus::SO3<double>;
+using SE3  = Sophus::SE3<double>;
+using Sim3 = Sophus::Sim3<double>;
+
+using SO2 = Sophus::SO2<double>;
+using SE2 = Sophus::SE2<double>;
