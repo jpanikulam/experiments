cbc394a86ff2081a6c819568b5d6f9534d012bb0
Jacob Panikulam <jpanikul@gmail.com>
2018-04-30 00:40:41 -0400

    Clang still cannot find includes

--- clang/find_functions.py
+++ clang/find_functions.py
@@ -1,32 +1,80 @@
+import os
+
 from clang import cindex
+from clang.cindex import CursorKind
 import clang
 
-def visitor(cursor, go=False):
-    # if go:
-        # print 'visiting', cursor.kind
-    # if cursor.kind == clang.cindex.CursorKind.CALL_EXPR:
 
+kinds = set()
+
+
+def visitor(cursor, path, go=False):
     children = list(cursor.get_children())
     # print '  has {0} children'.format(len(children))
 
-    if cursor.kind == clang.cindex.CursorKind.FUNCTION_DECL:
-        print 'Found %s [line=%s, col=%s]' % (
-                cursor.displayname, cursor.location.line, cursor.location.column)
-        for child in children:
-            visitor(child, True)
+    if (cursor.kind not in kinds):
+        print cursor.kind, ':', cursor.location.file, cursor.spelling
 
-    if cursor.kind == clang.cindex.CursorKind.PARM_DECL:
-        print '\t', cursor.type.spelling, cursor.spelling
+    kinds.add(cursor.kind)
+
+    if cursor.kind == clang.cindex.CursorKind.INCLUSION_DIRECTIVE:
+        print 'ffff'
+        print cursor.spelling
+
+    if cursor.kind in [CursorKind.UNEXPOSED_EXPR, CursorKind.UNEXPOSED_DECL, CursorKind.TRANSLATION_UNIT]:
+        print cursor.kind, ':', cursor.location.file, cursor.spelling
+
+    if (cursor.location.file is not None):
+        if (os.path.realpath(cursor.location.file.name) != path):
+            return
+
+    if (cursor.location.file is not None and cursor.kind == clang.cindex.CursorKind.CXX_METHOD):
+        path = os.path.realpath(cursor.location.file.name)
+
+        print os.path.split(path)[-1], cursor.spelling
+        for child in children:
+            visitor(child, path)
 
     for child in children:
-        visitor(child)
+        visitor(child, path)
 
 
 if __name__ == '__main__':
     cindex.Config.set_library_path('/usr/lib/llvm-3.8/lib')
-    path = '/home/jacob/repos/NVIDIA_CUDA-8.0_Samples/2_Graphics/volumeRender/volumeRender_kernel.cu'
+    path = '/home/jacob/repos/experiments/geometry/spatial/bounding_volume_hierarchy.hh'
 
     index = cindex.Index.create()
-    tu = index.parse(path)
 
-    visitor(tu.cursor)
+    include_paths = [
+        "/home/jacob/repos/experiments/geometry/spatial",
+        "/home/jacob/repos/experiments/",
+        "/home/jacob/repos/third_party/Sophus",
+        "/home/jacob/repos/third_party/eigen3",
+    ]
+
+    include_args = map(lambda o: "-I" + o, include_paths)
+
+    tu = index.parse(path, args=include_args)
+
+    for incl in tu.get_includes():
+        name = os.path.realpath(incl.location.file.name)
+
+        if name.startswith('/home/jacob/repos/experiments/') and incl.depth <= 3:
+            # print incl.location.file.name, ': ', incl.source.name, ': ', incl.depth
+            location = os.path.split(incl.location.file.name)[-1]
+            source = os.path.split(incl.source.name)[-1]
+            include = os.path.split(incl.include.name)[-1]
+
+            outstr = "loc: {0: <50} src: {1: <50} incl: {2: <40} depth: {3: <10}".format(
+                location,
+                source,
+                include,
+                incl.depth,
+            )
+            print outstr
+
+    print '---'
+
+    visitor(tu.cursor, path)
+
+    print kinds
