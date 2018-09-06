0c4e7b8778acbaeb9f1259aed260f6053ce5afee
Jacob Panikulam <jpanikul@gmail.com>
2018-09-06 01:15:28 -0400

    Add triangle shape
    - Distance to triangle
    - Nearest point on triangle

---
+++ geometry/shapes/triangle.hh
@@ -0,0 +1,20 @@
+#include "util/optional.hh"
+
+#include "eigen.hh"
+
+namespace geometry {
+namespace shapes {
+
+using Vec3 = Eigen::Vector3d;
+
+//
+// [1] https://iquilezles.org/www/articles/distfunctions/distfunctions.htm
+double sd_triangle(const Vec3& point, const Vec3& a, const Vec3& b, const Vec3& c);
+
+// Can you guess what this function does?
+Vec3 find_closest_point_on_triangle(const Vec3& point,
+                                    const Vec3& a,
+                                    const Vec3& b,
+                                    const Vec3& c);
+}  // namespace shapes
+}  // namespace geometry
