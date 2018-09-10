6bd4805f60c6f01f0a3b1b9e98ca3c2a9393d93b
Jacob Panikulam <jpanikul@gmail.com>
2018-09-10 01:20:10 -0400

    Factor triangle mesh type

--- geometry/import/read_stl.hh
+++ geometry/import/read_stl.hh
@@ -2,19 +2,13 @@
 
 #include "eigen.hh"
 
+#include "geometry/tri_mesh.hh"
+
 #include <string>
 #include <vector>
 
 namespace geometry {
 namespace import {
-struct Triangle {
-  std::array<Eigen::Vector3d, 3> vertices;
-  Eigen::Vector3d normal;
-};
-
-struct TriMesh {
-  std::vector<Triangle> triangles;
-};
 
 TriMesh read_stl(const std::string &file_path);
 }
