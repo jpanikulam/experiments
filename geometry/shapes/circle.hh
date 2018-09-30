0c0b889f97a67ee42610e726830352c79ca0d52f
Jacob Panikulam <jpanikul@gmail.com>
2018-09-30 18:25:16 -0400

    Add circle and sphere shapes

---
+++ geometry/shapes/circle.hh
@@ -0,0 +1,14 @@
+#include "eigen.hh"
+
+#include "geometry/shapes/halfspace.hh"
+
+namespace geometry {
+namespace shapes {
+
+struct Circle {
+  Plane plane;
+  double radius;
+};
+
+}  // namespace shapes
+}  // namespace geometry
