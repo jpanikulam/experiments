28b24fe097a5fb5527d623feb633bac3406d5cd2
Jacob Panikulam <jpanikul@gmail.com>
2018-09-30 23:26:58 -0400

    Add function for computing a rotation between two vectors

---
+++ geometry/rotation_to.hh
@@ -0,0 +1,10 @@
+#include "eigen.hh"
+#include "sophus.hh"
+
+namespace geometry {
+
+// Lifted from [1]
+// [1] https://github.com/jpanikulam/sonder/blob/master/sonder/src/geometry/geometry.cc
+SO3 rotation_to(const Eigen::Vector3d &from, const Eigen::Vector3d &to);
+
+}  // namespace geometry
