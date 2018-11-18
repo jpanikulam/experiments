ce8f6e82077e089c9f57de709ff4698d1df7a995
Jacob Panikulam <jpanikul@gmail.com>
2018-11-18 16:25:34 -0500

    Put stl with materials

--- geometry/visualization/put_stl.cc
+++ geometry/visualization/put_stl.cc
@@ -10,13 +10,8 @@ namespace visualization {
 PutFunction create_put_stl(const std::string& path_to_stl) {
   const auto tri = geometry::import::read_stl(path_to_stl);
   const auto put = [tri](viewer::SimpleGeometry& geo, const SE3& body_from_world) {
-    const jcc::Vec4 color(0.8, 0.8, 0.8, 0.4);
-    for (size_t k = 0; k < tri.triangles.size(); ++k) {
-      const auto& this_tri = tri.triangles[k];
-      geo.add_line({this_tri.vertices[0], this_tri.vertices[1], color});
-      geo.add_line({this_tri.vertices[1], this_tri.vertices[2], color});
-      geo.add_line({this_tri.vertices[2], this_tri.vertices[0], color});
-    }
+    const jcc::Vec3 color(0.8, 0.8, 0.8);
+    geo.add_triangle_mesh({tri, color, true, 3.0, true});
   };
   return put;
 }
--- geometry/visualization/put_stl.hh
+++ geometry/visualization/put_stl.hh
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "sophus.hh"
 #include "viewer/primitives/simple_geometry.hh"
 
