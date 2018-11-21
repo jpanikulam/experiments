7db834df2ac19e35c2a8b066d218203ef1a66904
Jacob Panikulam <jpanikul@gmail.com>
2018-11-20 21:29:51 -0500

    Fix names in create_put_stl

--- geometry/visualization/put_stl.cc
+++ geometry/visualization/put_stl.cc
@@ -9,9 +9,9 @@ namespace visualization {
 
 PutFunction create_put_stl(const std::string& path_to_stl) {
   const auto tri = geometry::import::read_stl(path_to_stl);
-  const auto put = [tri](viewer::SimpleGeometry& geo, const SE3& body_from_world) {
+  const auto put = [tri](viewer::SimpleGeometry& geo, const SE3& world_from_body) {
     const jcc::Vec3 color(0.8, 0.8, 0.8);
-    geo.add_triangle_mesh({tri, body_from_world, color, true, 3.0, false});
+    geo.add_triangle_mesh({tri, world_from_body, color, true, 3.0, false});
   };
   return put;
 }
