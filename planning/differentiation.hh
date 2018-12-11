baa08589bb7bad8f3876ed9087bd453e5b78263e
Jacob Panikulam <jpanikul@gmail.com>
2018-12-11 01:24:36 -0500

    Cleanup differentiation

--- planning/differentiation.hh
+++ planning/differentiation.hh
@@ -22,7 +22,6 @@ class Differentiator {
   MatNd<Prob::X_DIM, Prob::X_DIM> state_jacobian(const State &x,
                                                  const ControlVec &u) const {
     const auto f_x = [this, &x, &u](const StateVec &dx) {
-      // TODO: Direct reference to jet
       const State xplus_dx = prob_.apply_delta(x, dx);
       const State xn = prob_.dynamics(xplus_dx, u);
       return prob_.delta_vec(xn, prob_.dynamics(x, u));
@@ -33,7 +32,6 @@ class Differentiator {
   MatNd<Prob::X_DIM, Prob::U_DIM> control_jacobian(const State &x,
                                                    const ControlVec &u) const {
     const auto f_u = [this, &x, &u](const ControlVec &du) {
-      // TODO: direct reference to jet
       return prob_.delta_vec(prob_.dynamics(x, u + du), prob_.dynamics(x, u));
     };
     return numerics::numerical_jacobian<Prob::X_DIM>(ControlVec::Zero().eval(), f_u);
@@ -49,6 +47,14 @@ class Differentiator {
     return numerics::numerical_hessian(StateVec::Zero().eval(), c_x);
   }
 
+  VecNd<Prob::X_DIM> state_gradient(const State &x, const ControlVec &u, int t) const {
+    const auto c_x = [this, t, &x, &u](const StateVec &dx) {
+      const State xplus_dx = prob_.apply_delta(x, dx);
+      return prob_.cost(xplus_dx, u, t);
+    };
+    return numerics::numerical_gradient(StateVec::Zero().eval(), c_x);
+  }
+
   MatNd<Prob::U_DIM, Prob::U_DIM> control_hessian(const State &x,
                                                   const ControlVec &u,
                                                   int t) const {
@@ -59,17 +65,7 @@ class Differentiator {
     return numerics::numerical_hessian(ControlVec::Zero().eval(), c_u);
   }
 
-  VecNd<Prob::X_DIM> state_gradient(const State &x, const ControlVec &u, int t) const {
-    const auto c_x = [this, t, &x, &u](const StateVec &dx) {
-      const State xplus_dx = prob_.apply_delta(x, dx);
-      return prob_.cost(xplus_dx, u, t);
-    };
-    return numerics::numerical_gradient(StateVec::Zero().eval(), c_x);
-  }
-
-  VecNd<Prob::U_DIM> control_gradient(const State &x,
-                                                  const ControlVec &u,
-                                                  int t) const {
+  VecNd<Prob::U_DIM> control_gradient(const State &x, const ControlVec &u, int t) const {
     const auto c_u = [this, t, &x, &u](const ControlVec &du) {
       //
       return prob_.cost(x, u + du, t);
