60c619d45aa2c3b121fc78b4701d5ff32eb79795
Jacob Panikulam <jpanikul@gmail.com>
2018-12-19 06:55:18 -0500

    Clean up jetsim

--- planning/jet/jetsim.cc
+++ planning/jet/jetsim.cc
@@ -35,43 +35,26 @@ void go() {
   setup();
   const auto view = viewer::get_window3d("Mr. Jet, jets");
 
-  std::cout << "First" << std::endl;
-
   const std::string jet_path = "/home/jacob/repos/experiments/data/jetcat_p160.stl";
   const auto put_jet = geometry::visualization::create_put_stl(jet_path);
 
-  std::cout << "Second" << std::endl;
   const auto jet_tree = view->add_primitive<viewer::SceneTree>();
 
   const auto jet_geo = view->add_primitive<viewer::SimpleGeometry>();
   const auto accum_geo = view->add_primitive<viewer::SimpleGeometry>();
 
-  for (int k = 0; k < 10000; ++k) {
-    accum_geo->add_point({jcc::Vec3::Random()});
-  }
-
-
-  std::cout << "Creating camera" << std::endl;
   const auto camera = std::make_shared<viewer::Camera>();
   view->add_camera(camera);
+
+  camera->set_world_from_camera(
+      SE3(SO3::exp(jcc::Vec3(3.0, 0.0, 0.0)), jcc::Vec3(0.0, 0.0, -2.0)));
 
   const JetModel model;
   model.insert(*jet_tree);
 
-  Parameters params;
-  params.mass = 100.0;
-  params.unit_z = jcc::Vec3::UnitZ();
-  // params.external_force = jcc::Vec3::UnitZ() * -1.0;
-
   State jet;
   jet.x = jcc::Vec3(-2.0, -2.0, 3.0);
 
-  // jet.v = jcc::Vec3(0.3, 0.1, -0.6);
-  // jet.w = jcc::Vec3::UnitX() * 0.2;
-
-  // jet.R_world_from_body = SO3::exp(jcc::Vec3(0.1, 3.5, 0.2));
-  // jet.w = jcc::Vec3::UnitX() * 0.01;
-  // jet.v = jcc::Vec3(0.0, 0.1, 0.1);
   jet.throttle_pct = 0.0;
 
   bool target_achieved = false;
@@ -94,28 +77,19 @@ void go() {
     }
     desire.target = target;
 
-    if (camera->have_image()){
-      std::cout << "Extracting" << std::endl;
+    if (camera->have_image()) {
       const cv::Mat image = camera->extract_image();
       cv::imshow("bababooie", image);
       cv::waitKey(10);
     }
 
     const auto future_states = plan(jet, desire, prev_controls);
-
-    {
-      prev_controls.clear();
-      for (std::size_t xu_ind = 1u; xu_ind < future_states.size(); ++xu_ind) {
-        const auto& xu = future_states.at(xu_ind);
-        // prev_controls.push_back(xu.control);
-      }
-    }
     for (std::size_t k = 0; k < future_states.size(); ++k) {
       const auto& state = future_states.at(k).state;
       const SE3 world_from_state = SE3(state.R_world_from_body, state.x);
       const double scale =
           static_cast<double>(k) / static_cast<double>(future_states.size());
-      // jet_geo->add_axes({world_from_state, 1.0 - scale});
+      jet_geo->add_axes({world_from_state, 1.0 - scale});
 
       if (k > 1) {
         jet_geo->add_line({future_states.at(k).state.x, future_states.at(k - 1).state.x,
@@ -132,23 +106,16 @@ void go() {
     std::cout << "\tv     : " << jet.v.transpose() << std::endl;
     std::cout << "\tthrust: " << jet.throttle_pct << std::endl;
 
-    // const State u = future_states.front();
-    // jet = rk4_integrate(jet, {}, params, dt);
-
     accum_geo->add_line({prev, jet.x, jcc::Vec4(1.0, 0.7, 0.7, 0.7), 5.0});
 
     const SE3 world_from_jet = SE3(jet.R_world_from_body, jet.x);
     // put_jet(*jet_geo, world_from_jet);
-
-    jet_tree->set_world_from_root(world_from_jet);
-
 
     jet_geo->add_line(
         {world_from_jet.translation(),
          world_from_jet.translation() +
              (world_from_jet.so3() * jcc::Vec3::UnitZ() * jet.throttle_pct * 0.1),
          jcc::Vec4(0.1, 0.9, 0.1, 0.8), 9.0});
-
 
     if (true) {
       const SO3 world_from_target_rot = SO3::exp(jcc::Vec3::UnitX() * 3.1415 * 0.5);
@@ -159,9 +126,10 @@ void go() {
       view->set_target_from_world(world_from_jet.inverse());
     }
 
+    jet_tree->set_world_from_root(world_from_jet);
     jet_geo->flip();
     accum_geo->flush();
-    // view->spin_until_step();
+    view->spin_until_step();
   }
 }
 
