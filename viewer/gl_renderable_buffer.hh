fb7655089036c3a27c0ac7e7997a74973df234ff
Jacob Panikulam <jpanikul@gmail.com>
2018-12-18 22:43:28 -0500

    Add renderable buffer (Needs work)

---
+++ viewer/gl_renderable_buffer.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "viewer/gl_size.hh"
+
+namespace viewer {
+class GlRenderableBuffer {
+ public:
+  GlRenderableBuffer(const GlSize& size);
+  void bind();
+
+ private:
+  void initialize();
+  bool initialized() const {
+    return initialized_;
+  }
+
+  int frame_buffer_id_ = -1;
+  int texture_target_ = -1;
+  int depth_buffer_ = -1;
+  bool initialized_ = false;
+
+  GlSize size_;
+};
+}  // namespace viewer
