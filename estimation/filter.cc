9fd3a2f3d5a21716cb315b98d1ab89c65acea13b
Jacob Panikulam <jpanikul@gmail.com>
2018-12-22 17:21:23 -0500

    Filter toward compiling

--- estimation/filter.cc
+++ estimation/filter.cc
@@ -1,111 +1,70 @@
-#include <any>
-#include <queue>
-
 #include "estimation/filter.hh"
 #include "estimation/time_point.hh"
 
+#include "numerics/numdiff.hh"
+#include "numerics/group_diff.hh"
+
 namespace estimation {
 
-template <State>
-class FilterUpdater {
- public:
-  FilterState<State> update_state(const FilterState<State>& xp,
-                                  const TimeDuration& dt) const {
-    const MatNd<State::DIM, State::DIM> A = diffs_.state_jacobian(xp.x);
-    const State x_new = dynamics_(x, to_seconds(dt));
-
-    const MatNd<State::DIM, State::DIM> P_new = A.transpose() * xp.P * A + (Q * dt);
-    return {x_new, P_new, xp.time_of_validity + to_duration(dt)};
-  }
-
- private:
-  Dynamics dynamics_;
-  Differentiator diffs_;
-
-  MatNd<State::DIM, State::DIM> Q = MatNd<State::DIM, State::DIM>::Identity();
-};
+// Returns the index of the added model
+template <typename State>
+int Ekf<State>::add_model(const Ekf<State>::ObservationModel& model) {
+  const int i = static_cast<int>(observation_models_.size());
+  observation_models_[i] = model;
+  return i;
+}
 
 template <typename State>
-class Ekf {
- private:
-  struct Measurement {
-    int type = -1;
-    std::any observation;
-    TimePoint time_of_validity;
-  };
-  struct Comparator {
-    bool operator()(const Measurement& a, const Measurement& b) const {
-      return a.time_of_validity > b.time_of_validity;
-    }
+FilterState<State> Ekf<State>::update_state(const FilterState<State>& xp,
+                                            const TimeDuration& dt) const {
+  const MatNd<State::DIM, State::DIM> Q = MatNd<State::DIM, State::DIM>::Identity();
+
+  const auto dynamics_fixed_dt = [this, dt](const State& x) {
+    return dynamics_(x, to_seconds(dt));
   };
 
-  using ObservationModel =
-      std::function<StateVec(const FilterState&, const std::any& obs)>;
+  const MatNd<State::DIM, State::DIM> A =
+      numerics::group_jacobian(xp.x, dynamics_fixed_dt);
 
-  // Returns the index of the added model
-  int add_model(const ObservationModel& model) {
-    const int i = static_cast<int>(observation_models_.size());
-    observation_models_[i] = model;
-    return i;
+  const State x_new = dynamics_fixed_dt(xp.x);
+
+  const MatNd<State::DIM, State::DIM> P_new = A.transpose() * xp.P * A + (Q * dt);
+  return {x_new, P_new, xp.time_of_validity + dt};
+}
+
+template <typename State>
+FilterState<State> Ekf<State>::dynamics_until(const FilterState<State>& x0,
+                                              const TimePoint& t) const {
+  constexpr double MAX_DT = 0.1;
+
+  FilterState<State> x = x0;
+  TimePoint time_simulated = x0.time_of_validity;
+  while (time_simulated < t) {
+    const TimeDuration dt = std::min(MAX_DT, t - time_simulated);
+    x = update_state(x, dt);
+    time_simulated += dt;
+  }
+  return x;
+}
+
+template <typename State>
+FilterState<State> Ekf<typename State>::service_all_measurements(
+    const FilterState<State>& x_hat0) const {
+  FilterState<State> x_hat = x_hat0;
+  while (!measurements_.empty()) {
+    const Measurement meas = measurements_.top();
+    x_hat = dynamics_until(x_hat, meas.time_of_validity);
+
+    const int i = meas.type;
+    const auto& observer = observation_models_.at(i);
+    const FilterStateUpdate<State> update = observer(x_hat, meas.observation);
+    x_hat.x += update.dx;
+    x_hat.P = update.P;
+
+    measurements_.pop();
   }
 
- public:
-  using MinQueue = std::priority_queue<Observation, std::vector, Comparator>;
-
-  Ekf(const TimePoint& start_time, const FilterState& initial_state) {
-    last_time_serviced_ = start_time;
-    filter_state_ = initial_state;
-  }
-
-  template <typename MeasurementType>
-  int add_model(const std::function<StateVec(const FilterState&,
-                                             const MeasurementType& meas)>& fnc) {
-    const auto type_erased_fnc = [](const FilterState& x, const std::any& obs) {
-      return fnc(x, std::any_cast<MeasurementType>(obs));
-    };
-    return add_model(type_erased_fnc);
-  }
-
-
-  void measure(const Measurement& measurement) {
-    measurements_.push(measurement);
-  }
-
-  FilterState dynamics_until(const FilterState& x0, const TimePoint& t) const {
-    constexpr double MAX_DT = 0.1;
-
-    FilterState x = x0;
-    TimePoint time_simulated = x0.time_of_validity;
-    while (time_simulated < t) {
-      const TimeDuration dt = std::min(MAX_DT, t - time_simulated);
-      x = updater_.update_state(x, dt);
-      time_simulated += dt;
-    }
-    return x;
-  }
-
-  FilterState service_all_measurements() {
-    FilterState x_hat = filter_state_;
-    while (!measurements_.empty()) {
-      const Measurement meas = measurements_.top();
-      x_hat = dynamics_until(x_hat, meas.time_of_validity);
-      measurements_.pop();
-    }
-
-    filter_state_ = x_hat;
-    last_time_serviced_ = filter_state_.time_of_validity;
-
-    return x_hat;
-  }
-
- private:
-  std::map<int, ObservationModel> observation_models_;
-
-  MinQueue measurements_;
-
-  FilterUpdater<State> updater_;
-  FilterState filter_state_;
-  TimePoint last_time_serviced_;
-};
+  return x_hat;
+}
 
 }  // namespace estimation
--- estimation/filter.hh
+++ estimation/filter.hh
@@ -1,16 +1,75 @@
 #pragma once
 
+#include "estimation/filter_state.hh"
+
+#include <any>
+#include <map>
+#include <queue>
+
 namespace estimation {
 
 template <typename State>
-struct FilterState {
-  // The actual state
-  State x;
+class Ekf {
+ private:
+  using StateVec = VecNd<State::DIM>;
 
-  // State-covariance
-  MatNd<State::X_DIM, State::X_DIM> P;
+  struct Measurement {
+    int type = -1;
+    std::any observation;
+    TimePoint time_of_validity;
+  };
 
-  // When this state was the estimate for the true state
-  TimePoint time_of_validity;
+  using ObservationModel =
+      std::function<StateVec(const FilterState<State>&, const std::any& obs)>;
+
+  // Returns the index of the added model
+  int add_model(const ObservationModel& model);
+
+ public:
+  using Dynamics = std::function<State(const State& x, double dt)>;
+
+  Ekf(const FilterState<State>& initial_state, const Dynamics& dynamics)
+      : filter_state_(initial_state), dynamics_(dynamics) {
+  }
+
+  FilterState<State> update_state(const FilterState<State>& xp,
+                                  const TimeDuration& dt) const;
+
+  template <typename MeasurementType>
+  int add_model(const std::function<StateVec(const FilterState<State>&,
+                                             const MeasurementType& meas)>& fnc) {
+    const auto type_erased_fnc = [fnc](const FilterState<State>& x, const std::any& obs) {
+      return fnc(x, std::any_cast<MeasurementType>(obs));
+    };
+    return add_model(type_erased_fnc);
+  }
+
+  template <typename MeasurementType>
+  void measure(const MeasurementType& observation,
+               const TimePoint& time_of_validity,
+               int measurement_id) {
+    measurements_.push({measurement_id, std::any_cast(observation), time_of_validity});
+  }
+
+  FilterState<State> dynamics_until(const FilterState<State>& x0,
+                                    const TimePoint& t) const;
+
+  FilterState<State> service_all_measurements(const FilterState<State>& x_hat0) const;
+
+ private:
+  struct Comparator {
+    bool operator()(const Measurement& a, const Measurement& b) const {
+      return a.time_of_validity > b.time_of_validity;
+    }
+  };
+  using MinQueue = std::priority_queue<Measurement, std::vector<Measurement>, Comparator>;
+  MinQueue measurements_;
+
+  std::map<int, ObservationModel> observation_models_;
+
+  FilterState<State> filter_state_;
+
+  const Dynamics dynamics_;
 };
+
 }  // namespace estimation
