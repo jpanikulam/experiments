bdc2b923aa0366f71b1d186773ab165f8e2da16d
Jacob Panikulam <jpanikul@gmail.com>
2018-12-23 17:19:09 -0500

    Add group-diff overload that uses ADL

--- numerics/group_diff.hh
+++ numerics/group_diff.hh
@@ -21,10 +21,19 @@ MatNd<Y::DIM, X::DIM> group_jacobian(const VecNd<X::DIM>& x,
                                      const ComputeDelta<Y>& compute_delta,
                                      const ApplyDelta<X>& apply_delta) {
   const auto f_x = [&apply_delta, &compute_delta, &fnc, &x](const VecNd<X::DIM>& dx) {
-
     const X xplus_dx = apply_delta(x, dx);
     return compute_delta(fnc(xplus_dx), fnc(x));
   };
   return numerical_jacobian<Y::DIM>(X::Zero().eval(), f_x);
 }
+
+template <typename X, typename Y>
+MatNd<Y::DIM, X::DIM> group_jacobian(const VecNd<X::DIM>& x, const GroupFnc<X, Y>& fnc) {
+  const auto f_x = [&fnc, &x](const VecNd<X::DIM>& dx) {
+    const X xplus_dx = apply_delta(x, dx);
+    return compute_delta(fnc(xplus_dx), fnc(x));
+  };
+  return numerical_jacobian<Y::DIM>(X::Zero().eval(), f_x);
+}
+
 }  // namespace numerics
