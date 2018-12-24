02331e1735eec250885dde41844d439aff63c22d
Jacob Panikulam <jpanikul@gmail.com>
2018-12-24 17:10:55 -0500

    Add deps to pymake pip

--- pymake/setup.py
+++ pymake/setup.py
@@ -10,11 +10,16 @@ setuptools.setup(
     license='MIT',
     long_description=__doc__,
     url='https://github.com/jpanikulam/experiments/tree/master/pymake',
+    author='Jacob Panikulam',
     author_email='jpanikul@gmail.com',
     packages=setuptools.find_packages(),
     description=description,
     keywords="cmake make stupid",
     platforms='any',
     zip_safe=True,
-    scripts=['pymake']
+    scripts=['pymake'],
+    install_requires=[
+        'colorama==0.3.9',
+    ],
+
 )
