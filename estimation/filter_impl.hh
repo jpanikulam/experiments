b2c0e4f8489e825705c0afeed470b2a86121f513
Jacob Panikulam <jpanikul@gmail.com>
2018-12-25 17:14:34 -0500

    Fix fat-finger in dynamics covariance update

--- estimation/filter_impl.hh
+++ estimation/filter_impl.hh
@@ -19,7 +19,8 @@ int Ekf<State>::add_model_pr(const Ekf<State>::AnyObservationModel& model) {
 template <typename State>
 FilterState<State> Ekf<State>::update_state(const FilterState<State>& xp,
                                             const TimeDuration& dt) const {
-  const MatNd<State::DIM, State::DIM> Q = MatNd<State::DIM, State::DIM>::Identity();
+  const MatNd<State::DIM, State::DIM> Q =
+      MatNd<State::DIM, State::DIM>::Identity() * 0.1;
 
   const auto dynamics_fixed_dt = [this, dt](const State& x) -> State {
     return dynamics_(x, to_seconds(dt));
@@ -31,7 +32,7 @@ FilterState<State> Ekf<State>::update_state(const FilterState<State>& xp,
   const State x_new = dynamics_fixed_dt(xp.x);
 
   const MatNd<State::DIM, State::DIM> P_new =
-      A.transpose() * xp.P * A + (Q * to_seconds(dt));
+      A * xp.P * A.transpose() + (Q * to_seconds(dt));
   return {x_new, P_new, xp.time_of_validity + dt};
 }
 
@@ -57,14 +58,18 @@ FilterState<State> Ekf<State>::service_all_measurements(
   while (!measurements_.empty()) {
     const Measurement meas = measurements_.top();
     x_hat = dynamics_until(x_hat, meas.time_of_validity);
+    std::cout << "tov: " << to_seconds(meas.time_of_validity.time_since_epoch())
+              << std::endl;
 
     const int i = meas.type;
     const auto& observer = observation_models_.at(i);
     const FilterStateUpdate<State> update = observer(x_hat, meas.observation);
+
     x_hat.x = apply_delta(x_hat.x, update.dx);
     x_hat.P = update.P_new;
 
-    std::cout << "x_hat.x.eps_dot: " << x_hat.x.eps_dot.transpose() << std::endl;
+    // std::cout << "Upd             : " << update.dx.transpose() << std::endl;
+    std::cout << "x_hat.x.eps_dot : " << x_hat.x.eps_dot.transpose() << std::endl;
     std::cout << "x_hat.x.eps_ddot: " << x_hat.x.eps_ddot.transpose() << std::endl;
 
     measurements_.pop();
