d57df4906d1a56333b229799a888cab6ac480125
Jacob Panikulam <jpanikul@gmail.com>
2018-12-26 16:43:00 -0500

    Get acausal-opt compiling

--- estimation/optimization/acausal_optimizer.hh
+++ estimation/optimization/acausal_optimizer.hh
@@ -1,7 +1,13 @@
 #pragma once
 
 #include <map>
+#include <vector>
+#include <any>
 
+#include "estimation/optimization/block_sparse_matrix.hh"
+#include "estimation/time_point.hh"
+
+#include "util/heap.hh"
 #include "out.hh"
 
 namespace estimation {
@@ -14,34 +20,41 @@ struct ProblemType {
 };
 
 template <typename Prob>
-struct Solution {
-  std::vector<Prob::State> x;
-  Prob::Parameters p;
+struct OptimizerSolution {
+  std::vector<typename Prob::State> x;
+  typename Prob::Parameters p;
 };
 
 template <typename Prob>
 class AcausalOptimizer {
+  using State = typename Prob::State;
+  using Parameters = typename Prob::Parameters;
+  using Solution = OptimizerSolution<Prob>;
+
  public:
+  using Dynamics = std::function<State(const State&, const Parameters&, double dt)>;
+
+  template <typename Observation>
+  using ErrorModel = std::function<VecNd<Observation::DIM>(
+      const State&, const Observation&, const Parameters&)>;
+
+  using TypelessErrorModel =
+      std::function<VecXd(const State& x, const std::any& z, const Parameters& p)>;
+
   struct Measurement {
     int type = -1;
     std::any observation;
     TimePoint time_of_validity;
   };
 
-  using Dynamics = std::function<State(const State&, const Parameters&, double dt)>;
-
-  template <typename Observation>
-  using ErrorModel = std::function<VecNd<Observation::DIM>(
-      const Prob::State&, const Observation&, const Prob::Parameters&)>;
-
-  using TypelessErrorModel = std::function<VecXd(
-      const Prob::State& x, const std::any& z, const Prob::Parameters& p)>;
+  AcausalOptimizer(const Dynamics& dynamics) : dynamics_(dynamics) {
+  }
 
   template <typename Observation>
   int add_error_model(const ErrorModel<Observation>& model) {
     int model_id = models_.size();
-    const auto type_erased_model = [model](const Prob::State& x, const std::any& z,
-                                           const Prob::Parameters& p) {
+    const auto type_erased_model = [model](const State& x, const std::any& z,
+                                           const Parameters& p) {
       const VecNd<Observation::DIM> error = model(x, std::any_cast<Observation>(z), p);
       return VecXd(error);
     };
@@ -64,26 +77,7 @@ class AcausalOptimizer {
                                 int x_ind,
                                 int residual_ind,
                                 int param_ind,
-                                Out<BlockSparseMatrix> bsm) {
-    // y = (z[t] - h(x[t]; p))
-    // J[obs_ind,  state_ind] = dy/dx[t] = -H
-    // J[obs_ind, params_ind] = dy/dp    = -C
-
-    const auto model& = models_.at(z.type);
-    const auto y_of_x = [&model, &z, &p](const Prob::State& x) {
-      // Hold p
-      return model(x, z, p);
-    };
-    const auto y_of_p = [&model, &z, &x](const Prob::Parameters& p) {
-      // Hold x
-      return model(x, z, p);
-    };
-
-    const MatXd dy_dx = dynamic_group_jacobian(x_0, y_of_x);
-    const MatXd dy_dp = dynamic_group_jacobian(p, y_of_p);
-    bsm->set(residual_ind, x_ind, dy_dx);
-    bsm->set(residual_ind, param_ind, dy_dp);
-  }
+                                Out<BlockSparseMatrix> bsm);
 
   void add_dynamics_residual(const State& x_0,
                              const State& x_1,
@@ -92,78 +86,19 @@ class AcausalOptimizer {
                              int x_ind,
                              int residual_ind,
                              int param_ind,
-                             Out<BlockSparseMatrix> bsm) {
-    // y = (x[t+1] - f(x[t]; p)
-    // J[obs_ind,          t] = dy/dx[t]   = -A
-    // J[obs_ind, params_ind] = dy/dp      = -G
-    // J[obs_ind,        t+1] = dy/dx[t+1] = I
+                             Out<BlockSparseMatrix> bsm);
 
-    const int block_type = 0;
-    const auto model& = dynamics_;
-    const auto y_of_p = [dt, &model, &x_0, &x_1](const Prob::Parameters& p) {
-      // Hold p
-      return compute_delta(x_1, model(x_0, p, dt));
-    };
-
-    const auto y_of_x = [dt, &model, &x_0, &x_1](const Prob::Parameters& p) {
-      // Hold x1
-      return compute_delta(x_1, model(x_0, p, dt));
-    };
-
-    const MatXd dy_dx = dynamic_group_jacobian(x_0, y_of_x);
-    const MatXd dy_dp = dynamic_group_jacobian(p, y_of_p);
-
-    using StateToStateJac = MatNd<State::DIM, State::DIM>;
-    const StateToStateJac I = StateToStateJac::Identity();
-
-    bsm->set(residual_ind, x_ind, dy_dx);
-    bsm->set(residual_ind, x_ind + 1, I);
-    bsm->set(residual_ind, param_ind, dy_dp);
-  }
-
-  Solution solve(const Solution& initialization) {
-    assert(initialization.size() == heap_.size());
-    Solution<Prob> soln = initialization;
-
-    constexpr int n_params = 1;
-    const int n_measurements = static_cast<int>(heap_.size());
-    const int n_states = static_cast<int>(initialization.size());
-
-    BlockSparseMatrix J(n_states + n_measurements, n_states + params);
-
-    // Immediately after the states
-    const int p_ind = n_states;
-
-    const auto& measurements = heap_.backing();
-    for (int t = 0; t < static_cast<int>(measurements.size()) - 1) {
-      const auto& z_t = measurements[t];
-
-      const double dt =
-          to_seconds(measurements[t + 1].time_of_validity - z_t.time_of_validity);
-      // TODO: Do something special if dt == 0 (Share a measurement on a state?)
-
-      // TODO: Divide information by dt
-      constexpr int z_ind = 2 * t;
-      x_t.at(t);
-      add_observation_residual(x_t, z_t, p, dt, x_ind, z_ind, p_ind, out(J));
-
-      x_t1 = soln.at(t + 1);
-      add_dynamics_residual(x_t, x_t1, p, dt, x_ind, z_ind + 1, p_ind, out(J));
-    }
-
-    // JtJ;
-
-    // Jtv;
-  }
+  Solution solve(const Solution& initialization);
 
  private:
   Dynamics dynamics_;
-  std::map<int, TypelessErrorModel>;
+  std::map<int, TypelessErrorModel> models_;
 
   // Min-Heap
-  Heap<Measurement> heap_([](const Measurement& a, const Measurement& b) {
-    return a.time_of_validity > b.time_of_validity;
-  };);
+  Heap<Measurement> heap_ =
+      Heap<Measurement>([](const Measurement& a, const Measurement& b) {
+        return a.time_of_validity > b.time_of_validity;
+      });
 };
 
 }  // namespace optimization
--- estimation/optimization/acausal_optimizer_impl.hh
+++ estimation/optimization/acausal_optimizer_impl.hh
@@ -18,18 +18,18 @@ void AcausalOptimizer<Prob>::add_observation_residual(const State& x,
   // J[obs_ind,  state_ind] = dy/dx[t] = -H
   // J[obs_ind, params_ind] = dy/dp    = -C
 
-  const auto model& = models_.at(z.type);
-  const auto y_of_x = [&model, &z, &p](const Prob::State& x) {
+  const auto& model = models_.at(z.type);
+  const auto y_of_x = [&model, &z, &p](const State& x) {
     // Hold p
     return model(x, z, p);
   };
-  const auto y_of_p = [&model, &z, &x](const Prob::Parameters& p) {
+  const auto y_of_p = [&model, &z, &x](const Parameters& p) {
     // Hold x
     return model(x, z, p);
   };
 
-  const MatXd dy_dx = dynamic_group_jacobian(x_0, y_of_x);
-  const MatXd dy_dp = dynamic_group_jacobian(p, y_of_p);
+  const MatXd dy_dx = numerics::dynamic_group_jacobian<State>(x, y_of_x);
+  const MatXd dy_dp = numerics::dynamic_group_jacobian<Parameters>(p, y_of_p);
   bsm->set(residual_ind, x_ind, dy_dx);
   bsm->set(residual_ind, param_ind, dy_dp);
 }
@@ -48,20 +48,19 @@ void AcausalOptimizer<Prob>::add_dynamics_residual(const State& x_0,
   // J[obs_ind, params_ind] = dy/dp      = -G
   // J[obs_ind,        t+1] = dy/dx[t+1] = I
 
-  const int block_type = 0;
-  const auto model& = dynamics_;
-  const auto y_of_p = [dt, &model, &x_0, &x_1](const Prob::Parameters& p) {
-    // Hold p
+  const auto& model = dynamics_;
+  const auto y_of_x = [dt, &model, &x_1, &p](const State& x) {
+    // Hold p, x_1
+    return compute_delta(x_1, model(x, p, dt));
+  };
+
+  const auto y_of_p = [dt, &model, &x_0, &x_1](const Parameters& p) {
+    // Hold x_0, x_1
     return compute_delta(x_1, model(x_0, p, dt));
   };
 
-  const auto y_of_x = [dt, &model, &x_0, &x_1](const Prob::Parameters& p) {
-    // Hold x1
-    return compute_delta(x_1, model(x_0, p, dt));
-  };
-
-  const MatXd dy_dx = dynamic_group_jacobian(x_0, y_of_x);
-  const MatXd dy_dp = dynamic_group_jacobian(p, y_of_p);
+  const MatXd dy_dx = numerics::dynamic_group_jacobian<State>(x_0, y_of_x);
+  const MatXd dy_dp = numerics::dynamic_group_jacobian<Parameters>(p, y_of_p);
 
   using StateToStateJac = MatNd<State::DIM, State::DIM>;
   const StateToStateJac I = StateToStateJac::Identity();
@@ -72,22 +71,23 @@ void AcausalOptimizer<Prob>::add_dynamics_residual(const State& x_0,
 }
 
 template <typename Prob>
-AcausalOptimizer<Prob>::Solution AcausalOptimizer<Prob>::solve(
+typename AcausalOptimizer<Prob>::Solution AcausalOptimizer<Prob>::solve(
     const Solution& initialization) {
-  assert(initialization.size() == heap_.size());
-  Solution<Prob> soln = initialization;
+  assert(initialization.x.size() == heap_.size());
+  Solution soln = initialization;
 
   constexpr int n_params = 1;
   const int n_measurements = static_cast<int>(heap_.size());
-  const int n_states = static_cast<int>(initialization.size());
+  const int n_states = static_cast<int>(initialization.x.size());
 
-  BlockSparseMatrix J(n_states + n_measurements, n_states + params);
+  BlockSparseMatrix J(n_states + n_measurements, n_states + n_params);
 
   // Immediately after the states
   const int p_ind = n_states;
 
+  const auto& p = soln.p;
   const auto& measurements = heap_.backing();
-  for (int t = 0; t < static_cast<int>(measurements.size()) - 1) {
+  for (int t = 0; t < static_cast<int>(measurements.size()) - 1; ++t) {
     const auto& z_t = measurements[t];
 
     const double dt =
@@ -95,11 +95,12 @@ AcausalOptimizer<Prob>::Solution AcausalOptimizer<Prob>::solve(
     // TODO: Do something special if dt == 0 (Share a measurement on a state?)
 
     // TODO: Divide information by dt
-    constexpr int z_ind = 2 * t;
-    x_t.at(t);
-    add_observation_residual(x_t, z_t, p, dt, x_ind, z_ind, p_ind, out(J));
+    const int x_ind = t;
+    const int z_ind = 2 * t;
+    const State& x_t = soln.x.at(t);
+    add_observation_residual(x_t, z_t, p, x_ind, z_ind, p_ind, out(J));
 
-    x_t1 = soln.at(t + 1);
+    const State& x_t1 = soln.x.at(t + 1);
     add_dynamics_residual(x_t, x_t1, p, dt, x_ind, z_ind + 1, p_ind, out(J));
   }
 
