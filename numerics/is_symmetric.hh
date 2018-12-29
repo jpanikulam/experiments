3d62b8ba7477da0d26256703fce3568105a39364
Jacob Panikulam <jpanikul@gmail.com>
2018-12-29 00:16:10 -0500

    Slightly refactor is_symmetric

--- numerics/is_symmetric.hh
+++ numerics/is_symmetric.hh
@@ -5,6 +5,7 @@
 namespace numerics {
 template <int rows>
 bool is_symmetric(const MatNd<rows, rows>& mat, const double eps = 1e-5) {
-  return (mat.transpose() - mat).template lpNorm<Eigen::Infinity>() < eps;
+  const double symmetry_defect  = (mat.transpose() - mat).template lpNorm<Eigen::Infinity>();
+  return symmetry_defect < eps;
 }
 }  // namespace numerics
