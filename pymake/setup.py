d56da024cf372bcf90048597e5f9ba8c4e191e46
Jacob Panikulam <jpanikul@gmail.com>
2018-12-29 12:37:33 -0500

    Increment pymake version

--- pymake/setup.py
+++ pymake/setup.py
@@ -6,7 +6,7 @@ description = "Python Super CMake"
 
 setuptools.setup(
     name='generate_cmake',
-    version='1.3',
+    version='1.5',
     license='MIT',
     long_description=__doc__,
     url='https://github.com/jpanikulam/experiments/tree/master/pymake',
