22badf166b2ea441e953939463f751020b8c251b
Isaac Dykeman <ijdykeman@gmail.com>
2018-12-28 19:54:27 -0500

    Isaac aruco usable (#5)
    * Add aruco AR tags

--- planning/jet/jetsim.cc
+++ planning/jet/jetsim.cc
@@ -12,6 +12,9 @@
 #include "viewer/primitives/camera.hh"
 #include "viewer/primitives/image.hh"
 
+#include "estimation/vision/calibration.hh"
+#include "estimation/vision/fiducial_pose.hh"
+
 #include "util/environment.hh"
 
 #include "eigen.hh"
@@ -20,7 +23,7 @@
 namespace planning {
 namespace jet {
 
-constexpr bool SHOW_CAMERA = false;
+constexpr bool SHOW_CAMERA = true;
 constexpr bool SHOW_CAMERA_PROJECTION = true;
 constexpr bool WRITE_IMAGES = false;
 constexpr bool PRINT_STATE = true;
@@ -64,6 +67,23 @@ void put_camera_projection(viewer::SimpleGeometry& geo, const viewer::Camera& ca
   geo.add_line({bottom_right_ray(length), bottom_left_ray(length), red});
 }
 
+void draw_fidudical_detections(const cv::Mat localization_camera_image_rgb,
+                               const SE3 world_from_opengl_camera,
+                               std::shared_ptr<viewer::SimpleGeometry> geo) {
+  const auto opengl_camera_from_opencv_camera =
+      SE3(SO3::exp(Eigen::Vector3d(M_PI, 0, 0)), Eigen::Vector3d(0, 0, 0)) *
+      SE3(SO3::exp(Eigen::Vector3d(0, 0, M_PI / 2 * 0)), Eigen::Vector3d(0, 0, 0));
+  const auto world_from_camera =
+      world_from_opengl_camera * opengl_camera_from_opencv_camera;
+  auto world_from_markers = estimation::vision::get_world_from_marker_centers(
+      localization_camera_image_rgb, world_from_camera);
+
+  for (auto const& marker_detection_world_space : world_from_markers) {
+    geo->add_line({world_from_opengl_camera.translation(),
+                   marker_detection_world_space.world_from_marker.translation(),
+                   jcc::Vec4(1.0, 0.1, 0.7, 1), 5.0});
+  }
+
 }  // namespace
 
 void go() {
@@ -77,21 +97,27 @@ void go() {
   const auto jet_geo = view->add_primitive<viewer::SimpleGeometry>();
   const auto accum_geo = view->add_primitive<viewer::SimpleGeometry>();
 
+  const auto image_tree = view->add_primitive<viewer::SceneTree>();
   {
-    const auto image_tree = view->add_primitive<viewer::SceneTree>();
     const std::string fiducial_path = jcc::Environment::asset_path() + "fiducial.jpg";
     const cv::Mat fiducial_tag = cv::imread(fiducial_path);
-    const auto image = std::make_shared<viewer::Image>(fiducial_tag);
-    image_tree->add_primitive("root", SE3(), "fiducial_1", image);
+    const auto board_texture_image = std::make_shared<viewer::Image>(fiducial_tag, 1, 1);
+    // view->add_primitive(board_texture_image);
+    // image_tree->add_primitive("root", SE3(), "fiducial_1",
+    // board_texture_image);
 
+    const auto world_from_fiducial =
+        SE3(SO3::exp(jcc::Vec3(M_PI / 5, 0.5, 0.0)), jcc::Vec3(0.0, 3.0, 0.0));
+
+    image_tree->add_primitive("root", world_from_fiducial, "fiducial_2",
+                              board_texture_image);
     image_tree->add_primitive(
-        "root", SE3(SO3::exp(jcc::Vec3(1.0, 0.0, 0.0)), jcc::Vec3(-1.0, 0.0, 0.0)),
-        "fiducial_2", image);
+        "root", SE3(SO3::exp(jcc::Vec3(3.14 / 2, 0.0, 0.5)), jcc::Vec3(1.0, 3.0, 1.0)),
+        "fiducial_3", board_texture_image);
   }
 
   const auto camera = std::make_shared<viewer::Camera>();
   view->add_camera(camera);
-  camera->set_world_from_camera(SE3());
 
   const JetModel model;
   if constexpr (DRAW_VEHICLE) {
@@ -102,8 +128,11 @@ void go() {
   // Add camera projection to the scene tree
   //
 
+  const SO3 camera_sideways_from_camera_down = SO3::exp(jcc::Vec3(-M_PI / 2, 0.0, 0.0)) *
+                                               SO3::exp(jcc::Vec3(0.0, M_PI, 0.0)) *
+                                               SO3::exp(jcc::Vec3(0.0, 0, M_PI));
   const SE3 jet_from_camera =
-      SE3(SO3::exp(jcc::Vec3(0.1, -0.1, 0.0)), jcc::Vec3(0.1, 0.05, 0.1));
+      SE3(camera_sideways_from_camera_down, jcc::Vec3(0.1, 0.05, 0.1));
   if (SHOW_CAMERA_PROJECTION) {
     const auto camera_geo = std::make_shared<viewer::SimpleGeometry>();
     put_camera_projection(*camera_geo, *camera);
@@ -117,27 +146,35 @@ void go() {
 
   jet.throttle_pct = 0.0;
 
-  bool target_achieved = false;
   const jcc::Vec3 final_target(0.5, 0.5, 0.9);
-  const jcc::Vec3 intermediate_target = final_target + jcc::Vec3(0.0, 0.0, 1.0);
 
   std::vector<Controls> prev_controls;
-  for (int j = 0; j < 1000 && !view->should_close(); ++j) {
+  int j = 0;
+  while (!view->should_close()) {
+    ++j;
     const jcc::Vec3 prev = jet.x;
 
     //
     // Planning state
     //
 
-    if ((jet.x - intermediate_target).norm() < 0.25) {
-      target_achieved = true;
-    }
-    const jcc::Vec3 target = target_achieved ? final_target : intermediate_target;
     Desires desire;
-    if (target_achieved) {
-      desire.supp_v_weight = 600.0;
+    desire.target = final_target;
+    if (j > 100) {
+      desire.target = jcc::Vec3(0, 0, 0);
+    } else if (j > 300) {
+      desire.target = jcc::Vec3(0, -2, 0);
+    } else if (j > 500) {
+      desire.target = jcc::Vec3(1, 1, -1);
+    } else if (j > 800) {
+      desire.target = jcc::Vec3(-2, -6, -1);
+    } else if (j > 1000) {
+      desire.target = jcc::Vec3(-2, -6, -1);
+    } else if (j > 1300) {
+      desire.target = jcc::Vec3(3, -6, 3);
+    } else if (j > 1600) {
+      desire.target = jcc::Vec3(0, 0, 0);
     }
-    desire.target = target;
 
     //
     // Execute planning
@@ -168,6 +205,12 @@ void go() {
 
     // put_jet(*jet_geo, world_from_jet);
 
+    // const SO3 camera_sideways_from_camera_down =
+    //     SO3::exp(jcc::Vec3(-M_PI / 2, 0.0, 0.0)) * SO3::exp(jcc::Vec3(0.0, M_PI, 0.0))
+    //     * SO3::exp(jcc::Vec3(0.0, 0, M_PI));
+    // const SE3 jet_from_camera =
+    //     SE3(camera_sideways_from_camera_down, jcc::Vec3(0.1, 0.05, 0.1));
+
     if constexpr (VISUALIZE_TRAJECTORY) {
       for (std::size_t k = 0; k < future_states.size(); ++k) {
         const auto& state = future_states.at(k).state;
@@ -190,7 +233,7 @@ void go() {
     }
 
     if constexpr (TRACK_VEHICLE) {
-      const SO3 world_from_target_rot = SO3::exp(jcc::Vec3::UnitX() * 3.1415 * 0.5);
+      const SO3 world_from_target_rot = SO3::exp(jcc::Vec3::UnitX() * M_PI * 0.5);
       const SE3 world_from_target(world_from_target_rot, world_from_jet.translation());
       view->set_target_from_world(world_from_target.inverse());
     } else {
@@ -201,11 +244,27 @@ void go() {
     camera->set_world_from_camera(world_from_jet * jet_from_camera);
     accum_geo->flush();
 
-    const cv::Mat image = camera->extract_image();
+    const cv::Mat camera_image_rgb = camera->extract_image();
+
+    draw_fidudical_detections(camera_image_rgb, world_from_jet * jet_from_camera,
+                              jet_geo);
+
+
     if (SHOW_CAMERA) {
-      cv::imshow("Localization Camera", image);
+      cv::imshow("Localization Camera", camera_image_rgb);
       cv::waitKey(1);
     }
+
+    estimation::vision::CalibrationManager calibration_manager;
+
+    if (j % 10 == 0) {
+      calibration_manager.add_camera_image(camera_image_rgb);
+    }
+    if (calibration_manager.num_images_collected() % (200 / 15) == 0 &&
+        calibration_manager.num_images_collected() > 0) {
+      calibration_manager.calibrate();
+    }
+
     if (WRITE_IMAGES) {
       if (j == 0) {
         std::cout << "Projection: " << std::endl;
@@ -214,17 +273,17 @@ void go() {
       if (j % 1 == 0) {
         std::cout << "-----" << j << std::endl;
         std::cout << camera->get_projection().modelview_mat() << std::endl;
-        cv::imwrite("jet_image_" + std::to_string(j) + ".jpg", image);
+        cv::imwrite("jet_image_" + std::to_string(j) + ".jpg", camera_image_rgb);
       }
     }
-
     jet_geo->flip();
-
-    view->spin_until_step();
   }
 
+  view->spin_until_step();
   cv::destroyAllWindows();
 }
+
+}  // namespace
 
 }  // namespace jet
 }  // namespace planning
--- viewer/primitives/camera.cc
+++ viewer/primitives/camera.cc
@@ -9,17 +9,16 @@ namespace viewer {
 cv::Mat Camera::capture_framebuffer() const {
   constexpr int ORIGIN_X = 0;
   constexpr int ORIGIN_Y = 0;
-  constexpr int FORMAT = GL_RED;
+  constexpr int FORMAT = GL_BGR;
 
   constexpr int WIDTH = 640;
   constexpr int HEIGHT = 640;
 
   constexpr int TYPE = GL_UNSIGNED_BYTE;
 
-  uint8_t* out_data = new uint8_t[WIDTH * HEIGHT];
+  uint8_t* out_data = new uint8_t[WIDTH * HEIGHT * 3];
   glReadPixels(ORIGIN_X, ORIGIN_Y, WIDTH, HEIGHT, FORMAT, TYPE, out_data);
-
-  constexpr int IM_CV_TYPE = CV_8UC1;
+  constexpr int IM_CV_TYPE = CV_8UC3;
   const cv::Mat image(HEIGHT, WIDTH, IM_CV_TYPE, out_data);
   cv::Mat flipped;
   cv::flip(image, flipped, 0);
@@ -34,7 +33,7 @@ void Camera::prepare_view() const {
       (static_cast<double>(size_.width) / static_cast<double>(size_.height));
   constexpr double NEAR_CLIP = 0.001;
   constexpr double FAR_CLIP = 1000.0;
-  constexpr double FOV = 60.0;
+  constexpr double FOV = 90.0;
   gluPerspective(FOV, aspect_ratio, NEAR_CLIP, FAR_CLIP);
 
   glMatrixMode(GL_MODELVIEW);
