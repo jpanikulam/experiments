724ad6d51dd156b555d5ed6e73b6151333479274
Jacob Panikulam <jpanikul@gmail.com>
2019-06-06 16:56:28 -0400

    More template jello

--- eigen_helpers.hh
+++ eigen_helpers.hh
@@ -9,7 +9,8 @@ namespace jcc {
 template <int a_rows, int b_rows, int cols>
 MatNd<a_rows + b_rows, cols> vstack(const MatNd<a_rows, cols> &a, const MatNd<b_rows, cols> &b) {
   MatNd<a_rows + b_rows, cols> ab;
-  ab << a, b;
+  ab.template topRows<a_rows>() = a;
+  ab.template bottomRows<b_rows>() = b;
   return ab;
 }
 
@@ -24,7 +25,8 @@ MatNd<rows, a_cols + b_cols> hstack(const MatNd<rows, a_cols> &a, const MatNd<ro
 template <int cols>
 VecNd<cols + 1> augment(const VecNd<cols> &a, const double val) {
   VecNd<cols + 1> v;
-  v << a, val;
+  v.template head<cols>() = a;
+  v[cols] = val;
   return v;
 }
 }
