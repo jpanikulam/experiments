b90134be9f53bf059c6d543ddbfea5f3237a5132
Jacob Panikulam <jpanikul@gmail.com>
2019-06-14 01:46:48 -0400

    Add SmartTexture for drawing text

--- viewer/text/gl_text.cc
+++ viewer/text/gl_text.cc
@@ -1,133 +1,179 @@
-#include "logging/assert.hh'"
+#include "viewer/text/gl_text.hh"
+
+#include "logging/assert.hh"
+
+#include <memory>
 
 //%deps(freetype)
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
-namespace {
+namespace viewer {
 
-// TODO
-// - Finish this function
-// - Write a thing that dumps out character textures (But does tex lookup get expensive)
-// - Generate text and draw lines to it in a vertical box
+class SmartTexture::ManagedTexture {
+ public:
+  // ManagedTexture() = default;
+  ManagedTexture() {
+    glGenTextures(1, &texture_id_);
+  }
 
-// [1] https://learnopengl.com/In-Practice/Text-Rendering
-void generate_character() {
-  FT_Library ft;
-  JASSERT(FT_Init_FreeType(&ft), "Failed initialize freetype");
+  ~ManagedTexture() {
+    glDeleteTextures(1u, &texture_id_);
+  }
 
-  FT_Face face;
-  JASSERT(FT_New_Face(ft, "fonts/arial.ttf", 0, &face), "Failed to load freetype font");
+  void bind() const {
+    glBindTexture(GL_TEXTURE_2D, texture_id_);
+  }
 
-  // Test Load
-  FT_Set_Pixel_Sizes(face, 0, 48);
-  JASSERT(FT_Load_Char(face, 'X', FT_LOAD_RENDER), "Failed to load freetype glyph");
+ private:
+  GLuint texture_id_ = 0;
+};
+
+// Takes ownership over a GL pointer
+SmartTexture::SmartTexture(const jcc::Vec2& size) : size_(size) {
+  managed_texture_ = std::make_shared<ManagedTexture>();
 }
 
-void boop() {
-  for (GLubyte c = 0; c < 128; c++) {
+void SmartTexture::tex_image_2d(const GLenum target,
+                                const GLint level,
+                                const GLint internal_format,
+                                const GLsizei width,
+                                const GLsizei height,
+                                const GLint border,
+                                const GLenum format,
+                                const GLenum type,
+                                GLvoid* const data) const {
+  JASSERT(managed_texture_.get(), "Managed texture must be instantiated");
+  managed_texture_->bind();
+  glTexImage2D(target, level, internal_format, width, height, border, format, type, data);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
+
+void SmartTexture::draw() const {
+  managed_texture_->bind();
+
+  glBegin(GL_QUADS);
+  {
+    glTexCoord2d(0.0, 0.0);
+    glVertex2d(0.0, 0.0);
+
+    glTexCoord2d(0.0, 1.0);
+    glVertex2d(0.0, size_.y());
+
+    glTexCoord2d(1.0, 1.0);
+    glVertex2d(size_.x(), size_.y());
+
+    glTexCoord2d(1.0, 0.0);
+    glVertex2d(size_.x(), 0.0);
+  }
+  glEnd();
+}
+
+namespace {
+
+CharacterLibrary create_font_textures(const FT_Face& face) {
+  CharacterLibrary textures;
+
+  for (GLubyte character_id = 0; character_id < 128; character_id++) {
     // Load character glyph
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);  // Disable byte-alignment restriction
 
-    JASSERT(FT_Load_Char(face, c, FT_LOAD_RENDER),
-            "Failed to load glyph for a character");
+    if (FT_Load_Char(face, character_id, FT_LOAD_RENDER)) {
+      continue;
+    }
 
     // Generate texture
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(GL_TEXTURE_2D,
-                 0,
-                 GL_LUMINANCE,
-                 face->glyph->bitmap.width,
-                 face->glyph->bitmap.rows,
-                 0,
-                 GL_LUMINANCE,
-                 GL_UNSIGNED_BYTE,
-                 face->glyph->bitmap.buffer);
-    // Set texture options
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // GLuint tex_id;
+    // glGenTextures(1, &tex_id);
+    const jcc::Vec2 dimensions =
+        jcc::Vec2(face->glyph->bitmap.width, face->glyph->bitmap.rows);
+    SmartTexture smart_tex(dimensions);
 
-    // Now store character for later use
+    // GL_LUMINANCE_ALPHA
+    smart_tex.tex_image_2d(GL_TEXTURE_2D,
+                           0,
+                           GL_LUMINANCE,
+                           face->glyph->bitmap.width,
+                           face->glyph->bitmap.rows,
+                           0,
+                           GL_LUMINANCE,
+                           GL_UNSIGNED_BYTE,
+                           face->glyph->bitmap.buffer);
 
-    // Character character = {
-    //     texture, glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-    //     glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
-    //     face->glyph->advance.x};
-    // Characters.insert(std::pair<GLchar, Character>(c, character));
+    // Store character for later use
+
+    constexpr long int ADVANCE_PER_PX = 64;
+    JASSERT_GT(
+        face->glyph->advance.x, 0, "Jake assumed that 'advance' is strictly positive");
+    const auto advance =
+        static_cast<std::size_t>(face->glyph->advance.x / ADVANCE_PER_PX);
+    textures[character_id] = GlCharacter{
+        .texture = smart_tex,                                                     //
+        .dimensions = dimensions,                                                 //
+        .bearing = jcc::Vec2(face->glyph->bitmap_left, face->glyph->bitmap_top),  //
+        .advance = advance                                                        //
+    };
   }
 
-  FT_Done_Face(face);
-  FT_Done_FreeType(ft);
+  JASSERT_FALSE(
+      textures.empty(),
+      "Failed to load any character glyphs. Probably, we failed to find a font.");
+
+  return textures;
 }
 
 }  // namespace
 
-class GlText {
- public:
-  GlText(const std::string& text) {
-    constexpr int DEPTH = 3;
-    buffer_ = std::make_unique(new unsigned char[cols * rows * DEPTH])
-  }
+// [1] https://learnopengl.com/In-Practice/Text-Rendering
+CharacterLibrary create_text_library() {
+  FT_Library ft;
+  JASSERT_EQ(FT_Init_FreeType(&ft), 0, "Failed initialize freetype");
 
- private:
-  bool allocated_texture_ = false;
-  int cols_ = -1;
-  int rows_ = -1;
+  FT_Face face;
+  const std::string libre_mono =
+      "/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf";
+  JASSERT_EQ(
+      FT_New_Face(ft, libre_mono.c_str(), 0, &face), 0, "Failed to load freetype font");
 
-  // auto testData = std::unique_ptr<unsigned char[]>{ new unsigned char[16000] };
-  std::unique_ptr<unsigned char[]> buffer_;
+  // Test Load
+  FT_Set_Pixel_Sizes(face, 0, 48);
+  JASSERT_EQ(FT_Load_Char(face, 'X', FT_LOAD_RENDER), 0, "Failed to load freetype glyph");
 
-  double aspect_ratio = 1.0;
-  double width_m_ = 1.0;
-};
-void GlText::update_gl() const {
-  if (!allocated_texture_) {
-    glGenTextures(1, &texture_id_);
-    allocated_texture_ = true;
-  }
+  const auto characters = create_font_textures(face);
 
-  glBindTexture(GL_TEXTURE_2D, texture_id_);
-
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, cols_, rows_, 0, GL_BGR, GL_UNSIGNED_BYTE,
-               &data_);
+  FT_Done_Face(face);
+  FT_Done_FreeType(ft);
+  return characters;
 }
 
-void GlText::draw() const {
-  if (to_update_) {
-    update_gl();
-    to_update_ = false;
-  }
-
+void write_string(const std::string& text, const CharacterLibrary& lib) {
+  glPushAttrib(GL_ENABLE_BIT);
   glEnable(GL_TEXTURE_2D);
-  glBindTexture(GL_TEXTURE_2D, texture_id_);
 
-  glColor4d(1.0, 1.0, 1.0, alpha_);
-  glBegin(GL_QUADS);
+  for (std::size_t i = 0u; i < text.size(); ++i) {
+    const char c = text[i];
+    const auto character = lib.at(c);
 
-  const double aspect_ratio = image_.cols / static_cast<double>(image_.rows);
-
-  const double height = aspect_ratio * width_m_;
-
-  glTexCoord2d(1, 0);
-  glVertex3d(height - (height * 0.5), 0 - (width_m_ * 0.5), 0);
-  glTexCoord2d(1, 1);
-  glVertex3d(height - (height * 0.5), width_m_ - (width_m_ * 0.5), 0);
-  glTexCoord2d(0, 1);
-  glVertex3d(0 - (height * 0.5), width_m_ - (width_m_ * 0.5), 0);
-  glTexCoord2d(0, 0);
-  glVertex3d(0 - (height * 0.5), 0 - (width_m_ * 0.5), 0);
-
-  glEnd();
-
-  glDisable(GL_TEXTURE_2D);
+    // Debug stuff
+    /*
+    std::cout << "Drawing: " << c << std::endl;
+    std::cout << "\t"
+              << "Bearing: " << character.bearing.transpose() << std::endl;
+    std::cout << "\t"
+              << "Dims   : " << character.dimensions.transpose() << std::endl;
+    std::cout << "\t"
+              << "Advance: " << character.advance << std::endl;
+    std::cout << "\n---" << std::endl;
+    */
+    glTranslated(character.bearing.x(), -character.bearing.y(), 0.0);
+    character.texture.draw();
+    glTranslated(character.advance - character.bearing.x(), character.bearing.y(), 0.0);
+  }
+  glPopAttrib();
 }
+
+}  // namespace viewer
---
+++ viewer/text/gl_text.hh
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "eigen.hh"
+
+#include <GL/glew.h>
+
+#include <map>
+#include <memory>
+
+namespace viewer {
+
+class SmartTexture {
+ public:
+  SmartTexture() = default;
+  explicit SmartTexture(const jcc::Vec2& size);
+
+  void tex_image_2d(const GLenum target,
+                    const GLint level,
+                    const GLint internal_format,
+                    const GLsizei width,
+                    const GLsizei height,
+                    const GLint border,
+                    const GLenum format,
+                    const GLenum type,
+                    GLvoid* const data) const;
+
+  // The texture must be configured when this is called, but we expect it to
+  // be called frequently enough that we're wary about assertion overhead
+  void draw() const;
+
+  // Should we permit a way to build directly?
+  // explicit SmartTexture(const ByteBuffer);
+  // static make_texture(...);
+
+ private:
+  class ManagedTexture;
+  std::shared_ptr<ManagedTexture> managed_texture_;
+  jcc::Vec2 size_;
+};
+
+//        |      |____________ <- width
+// height |      |
+//        |      |
+//        | b_x  |
+//        | -^-- |
+//        |      |            |<- b_y
+//        |      |            |
+//        |
+//        |-------------------------> Advance
+//
+struct GlCharacter {
+  SmartTexture texture;
+  jcc::Vec2 dimensions;  // Size of glyph
+  jcc::Vec2 bearing;     // Offset from baseline to left/top of glyph
+  std::size_t advance;   // Offset to advance to next glyph
+};
+
+using CharacterLibrary = std::map<char, GlCharacter>;
+CharacterLibrary create_text_library();
+
+void write_string(const std::string& text, const CharacterLibrary& lib);
+
+}  // namespace viewer
