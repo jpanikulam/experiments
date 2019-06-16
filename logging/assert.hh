e6f22d88590aa8cf3bfba1dc759e5eb55e7ae0c7
Jacob Panikulam <jpanikul@gmail.com>
2019-06-16 14:13:02 -0400

    Add assert_between

--- logging/assert.hh
+++ logging/assert.hh
@@ -16,20 +16,26 @@ inline char* filename(char const* const argument) {
   return basename(const_cast<char*>(argument));
 }
 
+template <typename T>
+inline std::stringstream to_expr(const T& val, char const* const expr) {
+  std::stringstream ss;
+  return expr << " (" << val << ")";
+}
+
 template <typename A, typename B>
-inline std::stringstream expector_string(const A& a,
-                                         const B& b,
-                                         char const* const a_expr,
-                                         char const* const b_expr,
-                                         char const* const msg,
-                                         char const* const file_name,
-                                         int line_num,
-                                         char const* const operator_name) {
+inline std::stringstream binary_expector_string(const A& a,
+                                                const B& b,
+                                                char const* const a_expr,
+                                                char const* const b_expr,
+                                                char const* const msg,
+                                                char const* const file_name,
+                                                int line_num,
+                                                char const* const operator_name) {
   std::stringstream ss;
   std::cerr << Format::yellow() << filename(file_name) << ":" << line_num << ": "
-            << Format::red() << "Expected" << Format::reset() << ": " << a_expr << " ("
-            << a << ") " << operator_name << " " << b_expr << " (" << b << ")"
-            << ": " << msg;
+            << Format::red() << "Expected" << Format::reset() << ": "
+            << to_expr(a, a_expr) << operator_name << " " << to_expr(b, b_expr) << ": "
+            << msg;
   return ss;
 }
 
@@ -44,9 +50,10 @@ inline void assert_gt(const A& a,
                       char const* const file_name,
                       int line_num) {
   if (UNLIKELY(a <= b)) {
-    std::cerr
-        << expector_string(a, b, a_expr, b_expr, msg, file_name, line_num, ">").str()
-        << std::endl;
+    std::cerr << binary_expector_string(
+                     a, b, a_expr, b_expr, msg, file_name, line_num, ">")
+                     .str()
+              << std::endl;
     std::abort();
   }
 }
@@ -60,9 +67,10 @@ inline void assert_ge(const A& a,
                       char const* const file_name,
                       int line_num) {
   if (UNLIKELY(a < b)) {
-    std::cerr
-        << expector_string(a, b, a_expr, b_expr, msg, file_name, line_num, ">=").str()
-        << std::endl;
+    std::cerr << binary_expector_string(
+                     a, b, a_expr, b_expr, msg, file_name, line_num, ">=")
+                     .str()
+              << std::endl;
     std::abort();
   }
 }
@@ -76,9 +84,10 @@ inline void assert_lt(const A& a,
                       char const* const file_name,
                       int line_num) {
   if (UNLIKELY(a >= b)) {
-    std::cerr
-        << expector_string(a, b, a_expr, b_expr, msg, file_name, line_num, "<").str()
-        << std::endl;
+    std::cerr << binary_expector_string(
+                     a, b, a_expr, b_expr, msg, file_name, line_num, "<")
+                     .str()
+              << std::endl;
     std::abort();
   }
 }
@@ -91,9 +100,10 @@ inline void assert_le(const A& a,
                       char const* const file_name,
                       int line_num) {
   if (UNLIKELY(a > b)) {
-    std::cerr
-        << expector_string(a, b, a_expr, b_expr, msg, file_name, line_num, "<=").str()
-        << std::endl;
+    std::cerr << binary_expector_string(
+                     a, b, a_expr, b_expr, msg, file_name, line_num, "<=")
+                     .str()
+              << std::endl;
     std::abort();
   }
 }
@@ -107,9 +117,10 @@ inline void assert_eq(const A& a,
                       char const* const file_name,
                       int line_num) {
   if (UNLIKELY(a != b)) {
-    std::cerr
-        << expector_string(a, b, a_expr, b_expr, msg, file_name, line_num, "==").str()
-        << std::endl;
+    std::cerr << binary_expector_string(
+                     a, b, a_expr, b_expr, msg, file_name, line_num, "==")
+                     .str()
+              << std::endl;
 
     std::abort();
   }
@@ -124,9 +135,50 @@ inline void assert_ne(const A& a,
                       char const* const file_name,
                       int line_num) {
   if (UNLIKELY(a == b)) {
-    std::cerr
-        << expector_string(a, b, a_expr, b_expr, msg, file_name, line_num, "!=").str()
-        << std::endl;
+    std::cerr << binary_expector_string(
+                     a, b, a_expr, b_expr, msg, file_name, line_num, "!=")
+                     .str()
+              << std::endl;
+    std::abort();
+  }
+}
+
+template <typename A, typename B>
+inline void assert_between(const A& a,
+                           const B& b,
+                           const B& c,
+                           char const* const a_expr,
+                           char const* const b_expr,
+                           char const* const c_expr,
+                           char const* const msg,
+                           char const* const file_name,
+                           int line_num) {
+  if (UNLIKELY(((a < b) || (a > c)))) {
+    std::cerr << Format::yellow() << filename(file_name) << ":" << line_num << ": "
+              << Format::red() << "Expected" << Format::reset() << ": "
+              << to_expr(b, b_expr) << " <= " << to_expr(a, a_expr)
+              << " <= " << to_expr(c, c_expr) << ": " << msg;
+
+    std::abort();
+  }
+}
+
+template <typename A, typename B>
+inline void assert_outside(const A& a,
+                           const B& b,
+                           const B& c,
+                           char const* const a_expr,
+                           char const* const b_expr,
+                           char const* const c_expr,
+                           char const* const msg,
+                           char const* const file_name,
+                           int line_num) {
+  if (UNLIKELY(((a < b) || (a > c)))) {
+    std::cerr << Format::yellow() << filename(file_name) << ":" << line_num << ": "
+              << Format::red() << "Expected" << Format::reset() << ": "
+              << to_expr(b, b_expr) << " < " << to_expr(a, a_expr) << " < "
+              << to_expr(c, c_expr) << ": " << msg;
+
     std::abort();
   }
 }
@@ -164,6 +216,7 @@ inline void assert_false(const bool& a,
 //
 // Comparison Assertions
 //
+
 #define JASSERT_GE(a, b, msg) jcc::assert_ge(a, b, #a, #b, msg, __FILE__, __LINE__);
 #define JASSERT_GT(a, b, msg) jcc::assert_gt(a, b, #a, #b, msg, __FILE__, __LINE__);
 #define JASSERT_LE(a, b, msg) jcc::assert_le(a, b, #a, #b, msg, __FILE__, __LINE__);
@@ -172,8 +225,18 @@ inline void assert_false(const bool& a,
 #define JASSERT_NE(a, b, msg) jcc::assert_ne(a, b, #a, #b, msg, __FILE__, __LINE__);
 
 //
+// Ternary Assertions
+//
+
+#define JASSERT_BETWEEN(a, b, c, msg) \
+  jcc::assert_between(a, b, c, #a, #b, #c, msg, __FILE__, __LINE__);
+#define JASSERT_OUTSIDE(a, b, c, msg) \
+  jcc::assert_outside(a, b, c, #a, #b, #c, msg, __FILE__, __LINE__);
+
+//
 // Unary Assertions
 //
+
 #define JASSERT(a, msg) jcc::assert_true(a, #a, msg, __FILE__, __LINE__);
 #define JASSERT_TRUE(a, msg) jcc::assert_true(a, #a, msg, __FILE__, __LINE__);
 #define JASSERT_FALSE(a, msg) jcc::assert_false(a, #a, msg, __FILE__, __LINE__);
