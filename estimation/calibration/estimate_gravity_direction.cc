6a6619658187d1915bc1aff4e91acabfac37c923
Jacob Panikulam <jpanikul@gmail.com>
2019-06-30 00:18:07 -0400

    Lots of goofy fixing up of estimate_gravity

--- estimation/calibration/estimate_gravity_direction.cc
+++ estimation/calibration/estimate_gravity_direction.cc
@@ -31,12 +31,72 @@ jcc::Vec3 compute_biggest_difference(const std::vector<jcc::Vec3>& vecs) {
   for (const auto& v1 : vecs) {
     for (const auto& v2 : vecs) {
       const double cross = v1.cross(v2).norm();
+
       if (cross > biggest_cross) {
-        best = (v2 - v1);
+        best = v2.cross(v1);
+        biggest_cross = cross;
       }
     }
   }
   return best;
+}
+
+struct Bracket {
+  TimePoint start;
+  TimePoint end;
+};
+
+geometry::UnitVector3 estimate_from_bracket(const CalibrationMeasurements& measurements,
+                                            const ImuModel& imu_model,
+                                            const EstimationConfig& cfg,
+                                            const Bracket& bracket) {
+  constexpr double G_MPSS = 9.81;
+
+  JASSERT_GT(
+      estimation::to_seconds(bracket.end - bracket.start),
+      cfg.min_stationary_time_sec,
+      "Could not find a time bracket where the vehicle is stationary and pointed at "
+      "the fiducial for sufficiently long");
+
+  std::vector<double> bracket_norm_accel;
+  std::vector<jcc::Vec3> bracket_accel;
+
+  for (const auto& accel : measurements.accel_meas) {
+    if (accel.timestamp >= bracket.start && accel.timestamp <= bracket.end) {
+      const jcc::Vec3 corrected_accel =
+          imu_model.apply_calibration(accel.measurement.observed_acceleration);
+
+      if (std::abs(corrected_accel.norm() - G_MPSS) > cfg.max_g_defect) {
+        continue;
+      }
+
+      bracket_norm_accel.push_back(corrected_accel.norm());
+      bracket_accel.push_back(corrected_accel);
+    }
+  }
+
+  JASSERT_GT(bracket_accel.size(),
+             40u,
+             "Should have gotten at least 80 imu measurements in the window");
+
+  // These are not independent samples, but let's just work with it
+  const auto stats = numerics::compute_convenient_statistics(bracket_norm_accel);
+  JASSERT_LT(stats.max - stats.min,
+             cfg.max_accel_range_mpss,
+             "Accelerometer measurement range must be small");
+
+  JASSERT_LT(std::abs(stats.mean - G_MPSS),
+             cfg.max_g_defect,
+             "Mean was too far from g to be comfortable!");
+
+  const jcc::Vec3 biggest_error = compute_biggest_difference(bracket_accel);
+  const double max_angle_error = std::asin(biggest_error.norm());
+  JASSERT_LT(
+      max_angle_error, 0.01, "Accelerometer measurement angle range must be small");
+
+  const jcc::Vec3 average = numerics::compute_mean(bracket_accel);
+
+  return geometry::UnitVector3(average);
 }
 
 }  // namespace
@@ -50,16 +110,12 @@ geometry::UnitVector3 estimate_gravity_direction(
              1000u,
              "Should have at least 1000 accel measurements");
 
-  bool in_bracket = false;
-  TimePoint bracket_start = TimePoint::max();
-  TimePoint bracket_end = TimePoint::min();
-  std::vector<double> bracket_norm_acceleration;
-  std::vector<jcc::Vec3> bracket_acceleration;
-
-  // TimePoint best_bracket_start = std::numeric_limits::max();
-  // TimePoint best_bracket_end = std::numeric_limits::min();
-
   const auto first = measurements.fiducial_meas.front().timestamp;
+
+  bool in_bracket = false;
+  TimePoint bracket_start = first;
+  TimePoint bracket_end = first;
+
   for (std::size_t i = 1u; i < measurements.fiducial_meas.size(); ++i) {
     const auto prev = measurements.fiducial_meas.at(i - 1);
     const auto cur = measurements.fiducial_meas.at(i);
@@ -77,59 +133,34 @@ geometry::UnitVector3 estimate_gravity_direction(
     const jcc::Vec3 dx_dt = camera_1_from_camera_0.translation() / dt;
     const auto t = t0 + estimation::to_duration(dt * 0.5);
 
-    const bool valid = (dt < cfg.max_fiducial_dt_sec) &&  //
-                       (dx_dt.norm() < cfg.max_speed_mps);
-
     const auto accel = accel_interp(t);
+    const bool valid = (dt < cfg.max_fiducial_dt_sec) &&      //
+                       (dx_dt.norm() < cfg.max_speed_mps) &&  //
+                       accel;
 
     if (valid) {
-      if (accel) {
-        bracket_norm_acceleration.push_back(accel->norm());
-        bracket_acceleration.push_back(*accel);
-      }
-
       if (in_bracket) {
         bracket_end = t;
       } else {
         bracket_start = t;
+        bracket_end = t;
+        in_bracket = true;
       }
     } else {
-      bracket_norm_acceleration.clear();
-      bracket_acceleration.clear();
       in_bracket = false;
     }
 
-    if (estimation::to_seconds(bracket_end - bracket_start) > cfg.min_stationary_time) {
+    if (valid && in_bracket &&
+        (estimation::to_seconds(bracket_end - bracket_start) >
+         cfg.min_stationary_time_sec)) {
       break;
     }
   }
-  JASSERT_GT(
-      estimation::to_seconds(bracket_end - bracket_start),
-      cfg.min_stationary_time,
-      "Could not find a time bracket where the vehicle is stationary and pointed at "
-      "the fiducial for sufficiently long");
 
-  JASSERT_GT(accel_interp.count_between(bracket_start, bracket_end),
-             250u,
-             "Should have gotten at least 250 imu measurements in the window");
-  // These are not independent samples, but let's just work with it
-  const auto stats = numerics::compute_convenient_statistics(bracket_norm_acceleration);
-  JASSERT_LT(
-      stats.max - stats.min, 0.05, "Accelerometer measurement range must be small");
-  const jcc::Vec3 biggest_error = compute_biggest_difference(bracket_acceleration);
+  JASSERT(in_bracket, "Did not end inside a bracket, result is invalid.");
 
-  constexpr double G_MPSS = 9.81;
-  JASSERT_LT(std::abs(stats.mean - G_MPSS),
-             cfg.max_g_defect,
-             "Mean was too far from g to be comfortable!");
-
-  JASSERT_LT(std::asin(biggest_error.norm()),
-             0.01,
-             "Accelerometer measurement angle range must be small");
-
-  const jcc::Vec3 average = numerics::compute_mean(bracket_acceleration);
-
-  return geometry::UnitVector3(average);
+  const Bracket bracket{.start = bracket_start, .end = bracket_end};
+  return estimate_from_bracket(measurements, imu_model, cfg, bracket);
 }
 
 }  // namespace calibration
--- estimation/calibration/estimate_gravity_direction.hh
+++ estimation/calibration/estimate_gravity_direction.hh
@@ -10,8 +10,11 @@ namespace calibration {
 struct EstimationConfig {
   double max_speed_mps = 0.005;
   double max_fiducial_dt_sec = 0.2;
-  double min_stationary_time = 5.0;
-  double max_g_defect = 0.1;
+  double min_stationary_time_sec = 2.0;
+  double max_g_defect = 0.05;
+  // Largest permissible difference between max and min accel
+  // in the range where the vehicle appears to be stationary
+  double max_accel_range_mpss = 0.1;
 };
 
 geometry::UnitVector3 estimate_gravity_direction(
