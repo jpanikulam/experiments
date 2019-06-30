ac331fcb3f1136e81e662c44adef10350dd0acfc
Jacob Panikulam <jpanikul@gmail.com>
2019-06-29 20:41:36 -0400

    Add dataset struct for calibration

---
+++ estimation/calibration/calibration_dataset.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "estimation/jet/fiducial_measurement.hh"
+#include "estimation/jet/jet_rk4.hh"
+#include "estimation/time_point.hh"
+
+namespace estimation {
+namespace calibration {
+//
+
+template <typename MeasurementType>
+struct TimedMeasurement {
+  MeasurementType measurement;
+  TimePoint timestamp;
+};
+
+struct CalibrationMeasurements {
+  std::vector<TimedMeasurement<jet_filter::AccelMeasurement>> accel_meas;
+  std::vector<TimedMeasurement<jet_filter::GyroMeasurement>> gyro_meas;
+  std::vector<TimedMeasurement<jet_filter::FiducialMeasurement>> fiducial_meas;
+};
+
+}  // namespace calibration
+}  // namespace estimation
