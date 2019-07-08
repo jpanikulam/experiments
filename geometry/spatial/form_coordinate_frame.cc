6e4523a2ffb56182594ac813d5236c3af7b510ab
Jacob Panikulam <jpanikul@gmail.com>
2019-07-08 15:27:05 -0400

    Improve documentation

--- geometry/spatial/form_coordinate_frame.cc
+++ geometry/spatial/form_coordinate_frame.cc
@@ -7,9 +7,6 @@ namespace geometry {
 namespace spatial {
 
 SO3 form_coordinate_frame_from_zhat(const Unit3& zhat) {
-  // const jcc::Vec3 xhat = perp(zhat);
-  // const jcc::Vec3 yhat = zhat.vec().cross(xhat);
-
   const SO3 z_from_zhat = rotation_to(zhat.vec(), jcc::Vec3::UnitZ());
   return z_from_zhat;
 }
--- geometry/spatial/form_coordinate_frame.hh
+++ geometry/spatial/form_coordinate_frame.hh
@@ -6,11 +6,12 @@ namespace geometry {
 namespace spatial {
 
 // Form a coordinate frame that takes Z to provided zhat
-// Returns z_from_zhat
+// Returns world_from_frame
 SO3 form_coordinate_frame_from_zhat(const Unit3& zhat);
 
 // Forms a coordinate frame that takes Z to provided zhat,
 // and the x axis does its best to "point at" x_dir
+// Returns world_from_frame
 SO3 form_coordinate_frame_from_zhat_and_x(const Unit3& zhat, const jcc::Vec3& x_dir);
 
 }  // namespace spatial
