86c33b83fbf8635b105be577bbc3cb8e2b1f73d5
Jacob Panikulam <jpanikul@gmail.com>
2019-08-13 01:39:29 -0400

    Add a grid-search benchmark

--- gpgpu/demos/render.cc
+++ gpgpu/demos/render.cc
@@ -259,34 +259,65 @@ int main() {
   JCHECK_STATUS(ssd_kernel.setArg(3, dv_full_sum));
   {
     jcc::ScopedTimer tt;
-    for (float theta = 0.0f; theta < 100.14f; theta += 0.01f) {
-      const jcc::Vec3 axis(12.0 * std::cos(theta) * std::sin(theta), std::cos(theta),
-                           0.0);
-      const SE3 camera_from_plane(SO3::exp(axis), jcc::Vec3(-0.5, -0.5, 2.0));
-      cv::Mat out_img;
-      renderer.render(cmd_queue, camera_from_plane, dv_rendered_image, &out_img);
+    /*    for (float theta = 0.0f; theta < 100.14f; theta += 0.01f) {
+          const jcc::Vec3 axis(12.0 * std::cos(theta) * std::sin(theta), std::cos(theta),
+                               0.0);
+          const SE3 camera_from_plane(SO3::exp(axis), jcc::Vec3(-0.5, -0.5, 2.0));
+          cv::Mat out_img;
+          renderer.render(cmd_queue, camera_from_plane, dv_rendered_image, &out_img);
 
-      JCHECK_STATUS(
-          cmd_queue.enqueueNDRangeKernel(ssd_kernel, {0},
-                                         cl::NDRange(static_cast<std::size_t>(out_cols),
-                                                     static_cast<std::size_t>(out_rows)),
-                                         {480, 1}));
+          JCHECK_STATUS(
+              cmd_queue.enqueueNDRangeKernel(ssd_kernel, {0},
+                                             cl::NDRange(static_cast<std::size_t>(out_cols),
+                                                         static_cast<std::size_t>(out_rows)),
+                                             {480, 1}));
 
-      std::vector<float> partial_sums(n_partial_sums, 0.0f);
-      constexpr bool BLOCK_READ = true;
-      JCHECK_STATUS(cmd_queue.enqueueReadBuffer(
-          dv_partial_sums, BLOCK_READ, 0, n_partial_sum_bytes, partial_sums.data()));
+          std::vector<float> partial_sums(n_partial_sums, 0.0f);
+          constexpr bool BLOCK_READ = true;
+          JCHECK_STATUS(cmd_queue.enqueueReadBuffer(
+              dv_partial_sums, BLOCK_READ, 0, n_partial_sum_bytes, partial_sums.data()));
 
-      float blk_total;
-      JCHECK_STATUS(cmd_queue.enqueueReadBuffer(dv_full_sum, BLOCK_READ, 0,
-                                                sizeof(cl_float), &blk_total));
+          float blk_total;
+          JCHECK_STATUS(cmd_queue.enqueueReadBuffer(dv_full_sum, BLOCK_READ, 0,
+                                                    sizeof(cl_float), &blk_total));
 
-      geo->add_point({axis, jcc::Vec4((40.0 - blk_total) / 40.0, 0.5, 0.5, 1.0)});
-      geo->flush();
+          geo->add_point({axis, jcc::Vec4((40.0 - blk_total) / 40.0, 0.5, 0.5, 1.0)});
+          geo->flush();
 
-      out_img.convertTo(out_img, CV_8UC1);
-      // ui2d->add_image(out_img, 1.0);
-      // ui2d->flip();
+          out_img.convertTo(out_img, CV_8UC1);
+          // ui2d->add_image(out_img, 1.0);
+          // ui2d->flip();
+        }
+    */
+    for (float x = -10.0f; x < 10.0f; x += 0.1f) {
+      for (float y = -10.0f; y < 10.0f; y += 0.1f) {
+        const SE3 camera_from_plane(SO3::exp(ref_axis), jcc::Vec3(x, y, 2.0));
+        cv::Mat out_img;
+        renderer.render(cmd_queue, camera_from_plane, dv_rendered_image);
+
+        JCHECK_STATUS(cmd_queue.enqueueNDRangeKernel(
+            ssd_kernel, {0},
+            cl::NDRange(static_cast<std::size_t>(out_cols),
+                        static_cast<std::size_t>(out_rows)),
+            {480, 1}));
+
+        std::vector<float> partial_sums(n_partial_sums, 0.0f);
+        constexpr bool BLOCK_READ = true;
+        JCHECK_STATUS(cmd_queue.enqueueReadBuffer(
+            dv_partial_sums, BLOCK_READ, 0, n_partial_sum_bytes, partial_sums.data()));
+
+        float blk_total;
+        JCHECK_STATUS(cmd_queue.enqueueReadBuffer(dv_full_sum, BLOCK_READ, 0,
+                                                  sizeof(cl_float), &blk_total));
+
+        geo->add_point({camera_from_plane.translation(),
+                        jcc::Vec4((40.0 - blk_total) / 40.0, 0.5, 0.5, 1.0)});
+        geo->flush();
+
+        out_img.convertTo(out_img, CV_8UC1);
+        // ui2d->add_image(out_img, 1.0);
+        // ui2d->flip();
+      }
     }
   }
   view->spin_until_step();
@@ -303,11 +334,11 @@ of global memory reads
 
 This means we are re transferring 1.3GB/s
 This means we can perform the entire up/down transmit cycle with the GPU for a 500kb image
-once every *FOUR MICROSECONDS*
+once every *400 microseconds*
 
 If we don't transmit the image back, the rate is quadrupled. So for an optimization task
 where we don't need to inspect the rendered image on the CPU, we can generate an error
-every microsecond
+every 100 microseconds. That's two renders for every rotation of the jet turbine.
 
 Can tensorflow do this???
 
@@ -317,5 +348,9 @@ Contributing reasons:
 is simple and therefore very tempting
 
 NOTE: This isn't on jet hardware, but anyone familiar with GPU latency "myths" should be
-surprised by these numbers.
+surprised by these numbers for consumer hardware.
+
+On the jet, the story is a bit different:
+- The Mali GPU we have does not conventional VRAM on-device, it shares system main-memory
+- The Mali GPU we have is substantially less powerful
 */
