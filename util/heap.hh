29b35a9cfa0bad700bd8e4b2fdaa75c61010a6e9
Jacob Panikulam <jpanikul@gmail.com>
2019-08-13 00:51:37 -0400

    Add documentation to heap.hh

--- util/heap.hh
+++ util/heap.hh
@@ -1,3 +1,7 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Main Street Autonomy LLC.
+ ******************************************************************************/
+
 #pragma once
 
 #include "out.hh"
@@ -6,17 +10,21 @@
 #include <functional>
 #include <vector>
 
-// Max heap
+// Max heap wrapping the `std::make_heap` functionality
 template <typename T>
 class Heap {
  public:
   using CompareFunction = std::function<bool(const T &a, const T &b)>;
 
+  // Construct a max-heap with a custom comparator
+  // @param cmp The comparator to use (default: std::less)
+  //            Returns "true" when `a` is `less` than `b`
   Heap(const CompareFunction &cmp = [](const T &a,
                                        const T &b) { return std::less<T>()(a, b); })
       : cmp_(cmp) {
   }
 
+  // Construct a max-heap from an existing vector
   Heap(const std::vector<T> &vector,
        const CompareFunction &cmp = [](const T &a,
                                        const T &b) { return std::less<T>()(a, b); })
@@ -27,11 +35,13 @@ class Heap {
   Heap(const Heap<T> &o_heap) : cmp_(o_heap.cmp_), vector_(o_heap.vector_) {
   }
 
+  // Remove the drop element of the heap without any copies
   void drop() {
     std::pop_heap(vector_.begin(), vector_.end(), cmp_);
     vector_.pop_back();
   }
 
+  // Return a copy of the top (max) element of the heap, and delete it from the heap
   T pop() {
     std::pop_heap(vector_.begin(), vector_.end(), cmp_);
     const T result = vector_.back();
@@ -39,27 +49,34 @@ class Heap {
     return result;
   }
 
-  const T &top() {
+  // Return a reference to the top (max) element of the heap
+  const T &top() const {
     return vector_.front();
   }
 
+  // Add an element to the heap
+  // @param item The thing to be pushed to the heap
   void push(const T &item) {
     vector_.push_back(item);
     std::push_heap(vector_.begin(), vector_.end(), cmp_);
   }
 
+  // Remove all elements from the heap
   void clear() {
     vector_.clear();
   }
 
+  // @returns The number of elements in the heap
   size_t size() const {
     return vector_.size();
   }
 
+  // @returns True if the heap is empty
   bool empty() const {
     return vector_.empty();
   }
 
+  // Returns a sorted vector made from the elements of the heap
   std::vector<T> to_sorted_vector() const {
     Heap heap2 = *this;
     std::vector<T> result;
