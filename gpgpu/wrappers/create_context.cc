402be411a562ca791710127fab49f20c0c5410fa
Jacob Panikulam <jpanikul@gmail.com>
2019-08-16 13:14:44 -0400

    Default disable caching in context creation

--- gpgpu/wrappers/create_context.cc
+++ gpgpu/wrappers/create_context.cc
@@ -2,9 +2,14 @@
 
 #include "logging/assert.hh"
 
+#include <stdlib.h>
+
 namespace jcc {
 
 ClInfo create_context() {
+  // (globally, sorry): Disable CUDA caching
+  setenv("CUDA_CACHE_DISABLE", "1", 1);
+
   cl::Platform platform;
   std::vector<cl::Device> devices;
   platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
