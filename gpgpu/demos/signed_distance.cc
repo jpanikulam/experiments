627d17ffc521d2b4a834a3a1affa81b69a08c5b0
Jacob Panikulam <jpanikul@gmail.com>
2019-08-16 20:52:56 -0400

    More debug power in signed distance

--- gpgpu/demos/signed_distance.cc
+++ gpgpu/demos/signed_distance.cc
@@ -57,8 +57,9 @@ void draw(viewer::Window3D& view,
           const RenderConfig& cc_cfg,
           const ImageSize& out_im_size,
           float t) {
-  // const jcc::PrintingScopedTimer tt("Frame Time");
-  const SE3 world_from_camera = view.camera_from_world().inverse();
+  const jcc::PrintingScopedTimer tt("Frame Time");
+  const SE3 world_from_camera = view.standard_camera_from_world().inverse();
+
   const jcc::clSE3 cl_world_from_camera(world_from_camera);
 
   JCHECK_STATUS(sdf_kernel.setArg(2, cl_world_from_camera));
@@ -66,11 +67,8 @@ void draw(viewer::Window3D& view,
   const clRenderConfig cfg = cc_cfg.convert();
   JCHECK_STATUS(sdf_kernel.setArg(3, cfg));
 
-  cl_float cl_zoom = 0.0001f;
-  JCHECK_STATUS(sdf_kernel.setArg(4, cl_zoom));
-
   cl_float cl_t = t;
-  JCHECK_STATUS(sdf_kernel.setArg(5, cl_t));
+  JCHECK_STATUS(sdf_kernel.setArg(4, cl_t));
 
   const cl::NDRange work_group_size{static_cast<std::size_t>(out_im_size.cols),
                                     static_cast<std::size_t>(out_im_size.rows)};
@@ -86,7 +84,7 @@ void draw(viewer::Window3D& view,
   out_img.convertTo(out_img, CV_8UC4);
   cv::cvtColor(out_img, out_img, CV_BGRA2BGR);
 
-  // ui2d.add_image(out_img, 1.0);
+  ui2d.add_image(out_img, 1.0);
   ui2d.flip();
 }
 
@@ -97,12 +95,18 @@ int main() {
   view->set_elevation(0.0);
 
   const auto background = view->add_primitive<viewer::SimpleGeometry>();
-  const geometry::shapes::Plane ground{jcc::Vec3::UnitZ(), 0.0};
+  const geometry::shapes::Plane ground{jcc::Vec3(0.0, -0.7, -0.7).normalized(), -5.0};
+  // const geometry::shapes::Plane ground{jcc::Vec3(0.0, 1.0, 0.0).normalized(), 1.0};
   background->add_plane({ground});
   background->flip();
 
   const auto ui2d = view->add_primitive<viewer::Ui2d>();
   const auto geo = view->add_primitive<viewer::SimpleGeometry>();
+
+  geo->add_point({jcc::Vec3::UnitX(), jcc::Vec4(1.0, 0.0, 0.0, 1.0), 3.0});
+  geo->add_point({jcc::Vec3::UnitY(), jcc::Vec4(0.0, 1.0, 0.0, 1.0), 3.0});
+  geo->add_point({jcc::Vec3::UnitZ(), jcc::Vec4(0.0, 0.0, 1.0, 1.0), 3.0});
+  geo->flip();
 
   cl_int status = 0;
   const auto cl_info = jcc::create_context();
@@ -154,6 +158,7 @@ int main() {
   view->add_toggle_callback("terminal_iteration", [&](int iter_ct) {
     std::cout << "\tIteration Count --> " << iter_ct * 10 << std::endl;
     cc_cfg.terminal_iteration = iter_ct * 10;
+
     draw(*view, *ui2d, cmd_queue, sdf_kernel, dv_rendered_image, cc_cfg, out_im_size, t);
   });
 
@@ -182,9 +187,15 @@ int main() {
 
   view->run_menu_callbacks();
   view->trigger_continue();
+
   while (true) {
     t += 0.1;
     // cc_cfg.terminal_iteration = view->get_menu("terminal_iteration") * 10;
+
+    geo->add_sphere({jcc::Vec3(0.0, 0.0, 3.0), 0.5});
+    geo->add_sphere({jcc::Vec3(0.2, 0.0, 3.5), 1.0 + std::cos(0.05 * t)});
+    geo->add_sphere({jcc::Vec3(0.0, 0.0, 0.0), 0.1});
+    geo->flip();
 
     draw(*view, *ui2d, cmd_queue, sdf_kernel, dv_rendered_image, cc_cfg, out_im_size, t);
     view->spin_until_step();
