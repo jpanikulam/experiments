e8050d21b59fc63e36e8127dd5ad430f5b275416
Jacob Panikulam <jpanikul@gmail.com>
2019-08-24 01:03:00 -0400

    Add imgui manager for interacting with imgui

---
+++ viewer/interaction/imgui_manager.cc
@@ -0,0 +1,56 @@
+#include "viewer/interaction/imgui_manager.hh"
+
+#include "third_party/imgui/imgui.h"
+
+#include "third_party/imgui/examples/imgui_impl_glfw.h"
+#include "third_party/imgui/examples/imgui_impl_opengl2.h"
+
+// %deps(imgui)
+
+namespace viewer {
+
+void ImGuiManager::init(GLFWwindow* window) {
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  // TODO(jpanikulam): Handle multiple windows
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  (void)io;
+
+  // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard
+  // Controls io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable
+  // Gamepad Controls
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  // ImGui::StyleColorsClassic();
+
+  // Setup Platform/Renderer bindings
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL2_Init();
+}
+
+bool ImGuiManager::want_capture() const {
+  const ImGuiIO& io = ImGui::GetIO();
+  return io.WantCaptureMouse || io.WantCaptureKeyboard;
+}
+
+void ImGuiManager::new_frame() {
+  // Start the Dear ImGui frame
+  ImGui_ImplOpenGL2_NewFrame();
+  ImGui_ImplGlfw_NewFrame();
+  ImGui::NewFrame();
+}
+
+void ImGuiManager::render() {
+  ImGui::Render();
+  ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+}
+
+ImGuiManager::~ImGuiManager() {
+  ImGui_ImplOpenGL2_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+}
+
+}  // namespace viewer
---
+++ viewer/interaction/imgui_manager.hh
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+namespace viewer {
+
+//
+// How to use:
+//  1. Init (once) with a valid GLFWwindow
+//
+//  2. call new_frame()
+//  3. Call ImGui commands and create an imgui
+//  4. Call render() when done
+//
+// Note: You can use `want_capture()` to check if ImGui is
+// trying to capture the keyboard or mouse
+//
+//
+class ImGuiManager {
+ public:
+  void init(GLFWwindow* window);
+  void new_frame();
+  void render();
+  bool want_capture() const;
+  ~ImGuiManager();
+};
+
+}  // namespace viewer
