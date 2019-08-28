0ae7164962c9a06c0ae0085197eb8720331a5410
Jacob Panikulam <jpanikul@gmail.com>
2019-08-28 00:21:07 -0400

    Sort ray-intersected boxes by distance

--- planning/simulation/interactable_geometry.cc
+++ planning/simulation/interactable_geometry.cc
@@ -1,5 +1,7 @@
 #include "planning/simulation/interactable_geometry.hh"
 
+#include <algorithm>
+
 namespace jcc {
 namespace simulation {
 
@@ -13,12 +15,27 @@ std::vector<RayIntersection> InteractableGeometry::all_intersections(
           {bbox.first, intersection.distance, ray(intersection.distance)});
     }
   }
+  std::sort(intersections.begin(), intersections.end(),
+            [](const RayIntersection& a, const RayIntersection& b) -> bool {
+              return a.distance < b.distance;
+            });
+
   return intersections;
 }
 
 std::vector<BoundingBoxIntersection> InteractableGeometry::all_intersections(
-    const InteractableGeometry::BBox3& bbox) {
-  return {};
+    const InteractableGeometry::BBox3& other) {
+  std::vector<BoundingBoxIntersection> intersections;
+
+  for (const auto& bbox : aabb_) {
+    const auto intersection = bbox.second.bbox.intersect(other);
+
+    if (intersection.contained) {
+      intersections.push_back({bbox.first, intersection.contained});
+    }
+  }
+
+  return intersections;
 }
 
 }  // namespace simulation
