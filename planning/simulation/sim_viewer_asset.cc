080c799bf3d8c859a0baca8541e2074a62ef677c
Jacob Panikulam <jpanikul@gmail.com>
2019-09-03 17:41:39 -0400

    Always build a BVH for sim viewer assets

--- planning/simulation/sim_viewer_asset.cc
+++ planning/simulation/sim_viewer_asset.cc
@@ -1,10 +1,43 @@
 #include "planning/simulation/sim_viewer_asset.hh"
 
 #include "geometry/spatial/fit_bounding_box.hh"
+#include "geometry/spatial/triangle_volume.hh"
+
 #include "logging/assert.hh"
 
 namespace jcc {
 namespace simulation {
+
+struct ManagedBvh {
+  std::vector<geometry::spatial::TriangleVolume> triangles;
+  std::shared_ptr<geometry::spatial::BoundingVolumeHierarchy> bvh;
+};
+
+namespace {
+
+std::shared_ptr<ManagedBvh> build_bvh(const geometry::TriMesh& tri_mesh) {
+  auto mgd_bvh = std::make_shared<ManagedBvh>();
+  mgd_bvh->bvh = std::make_shared<geometry::spatial::BoundingVolumeHierarchy>();
+
+  // auto triangles = std::make_shared<std::vector<TriangleVolume>>();
+  mgd_bvh->triangles.reserve(tri_mesh.triangles.size());
+
+  std::vector<geometry::spatial::Volume*> tri_ptrs;
+  tri_ptrs.reserve(tri_mesh.triangles.size());
+
+  for (size_t k = 0; k < tri_mesh.triangles.size(); ++k) {
+    mgd_bvh->triangles.emplace_back(tri_mesh.triangles[k].vertices);
+    tri_ptrs.push_back(&mgd_bvh->triangles.back());
+  }
+
+  mgd_bvh->bvh->build(tri_ptrs);
+  return mgd_bvh;
+}
+}  // namespace
+
+std::shared_ptr<geometry::spatial::BoundingVolumeHierarchy> Asset::get_bvh() const {
+  return bvh->bvh;
+}
 
 Asset build_asset(const std::string& path) {
   const auto trimesh_optl = geometry::import::read_stl(path);
@@ -14,7 +47,7 @@ Asset build_asset(const std::string& path) {
   JASSERT(static_cast<bool>(trimesh_optl), error.c_str());
   const auto bbox = geometry::spatial::fit_bounding_box(*trimesh_optl);
 
-  return Asset{*trimesh_optl, bbox};
+  return Asset{*trimesh_optl, bbox, build_bvh(*trimesh_optl)};
 }
 
 }  // namespace simulation
--- planning/simulation/sim_viewer_asset.hh
+++ planning/simulation/sim_viewer_asset.hh
@@ -2,15 +2,22 @@
 
 #include "geometry/import/read_stl.hh"
 #include "geometry/spatial/bounding_box.hh"
+#include "geometry/spatial/bounding_volume_hierarchy.hh"
 
+#include <memory>
 #include <string>
 
 namespace jcc {
 namespace simulation {
 
+struct ManagedBvh;
+
 struct Asset {
   geometry::TriMesh mesh;
   geometry::spatial::BoundingBox<3> bbox;
+
+  std::shared_ptr<ManagedBvh> bvh;
+  std::shared_ptr<geometry::spatial::BoundingVolumeHierarchy> get_bvh() const;
 };
 
 Asset build_asset(const std::string& path);
