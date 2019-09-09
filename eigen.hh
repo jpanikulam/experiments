e0b190f3b2c9a78f073de4e0d0fdc1ec88b5489c
Jacob Panikulam <jpanikul@gmail.com>
2019-09-09 18:15:32 -0400

    Add float defs for eigen

--- eigen.hh
+++ eigen.hh
@@ -9,8 +9,17 @@ using VecNd = Eigen::Matrix<double, ROWS, 1>;
 template <int ROWS, int COLS>
 using MatNd = Eigen::Matrix<double, ROWS, COLS>;
 
+template <int ROWS>
+using VecNf = Eigen::Matrix<float, ROWS, 1>;
+
+template <int ROWS, int COLS>
+using MatNf = Eigen::Matrix<float, ROWS, COLS>;
+
 using VecXd = Eigen::VectorXd;
 using MatXd = Eigen::MatrixXd;
+
+using VecXf = Eigen::VectorXf;
+using MatXf = Eigen::MatrixXf;
 
 template <int ROWS>
 using SquareMatNd = Eigen::Matrix<double, ROWS, ROWS>;
@@ -25,6 +34,13 @@ using Vec3 = Eigen::Vector3d;
 using Vec4 = Eigen::Vector4d;
 using Vec5 = VecNd<5>;
 using Vec6 = VecNd<6>;
+
+using Vec1f = VecNf<1>;
+using Vec2f = Eigen::Vector2f;
+using Vec3f = Eigen::Vector3f;
+using Vec4f = Eigen::Vector4f;
+using Vec5f = VecNf<5>;
+using Vec6f = VecNf<6>;
 
 using Vec2i = Eigen::Matrix<int, 2, 1>;
 using Vec3i = Eigen::Matrix<int, 3, 1>;
