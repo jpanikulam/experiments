a48b13e1ac8daab037d761ccd1c93d80d5ed1cb1
Jacob Panikulam <jpanikul@gmail.com>
2019-09-28 16:15:53 -0400

    Imgui elements for debugging

--- rendering/imgui_elements/game_debug_configuration.hh
+++ rendering/imgui_elements/game_debug_configuration.hh
@@ -5,6 +5,8 @@ struct Debug {
   bool use_normals = false;
   bool use_rsm = false;
   bool wireframe = false;
+  double theta = 1.2;
+  double d = 1.2;
   int polygon_mode = 1;
 };
 struct GameDebugConfiguration {
--- rendering/imgui_elements/game_ui_elements.cc
+++ rendering/imgui_elements/game_ui_elements.cc
@@ -12,6 +12,20 @@ void show_menu(Out<GameDebugConfiguration> menu) {  ImGui::Begin("GameDebugConfi
 {    ImGui::Checkbox("Use Normals", &menu->debug.use_normals);
 }{    ImGui::Checkbox("Use Reflective Shadow Maps", &menu->debug.use_rsm);
 }{    ImGui::Checkbox("Use Wireframe", &menu->debug.wireframe);
+}{if (ImGui::Button("Reset##Theta")) {
+  menu->debug.theta = 1.2;
+}
+ImGui::SameLine();
+    float theta_tmp = static_cast<float>((menu->debug.theta));
+    ImGui::SliderFloat("Theta", &theta_tmp, 0.0, 3.14);
+    menu->debug.theta = static_cast<double>(theta_tmp);
+}{if (ImGui::Button("Reset##Distance")) {
+  menu->debug.d = 1.2;
+}
+ImGui::SameLine();
+    float d_tmp = static_cast<float>((menu->debug.d));
+    ImGui::SliderFloat("Distance", &d_tmp, 0.0, 5.0);
+    menu->debug.d = static_cast<double>(d_tmp);
 }{    const char* element_names[3] = {"GL_BACK", "GL_FRONT", "GL_FRONT_AND_BACK"};
     const char* current_element_name = (menu->debug.polygon_mode >= 0 && menu->debug.polygon_mode < 3) ? element_names[menu->debug.polygon_mode] : "Unknown";
     ImGui::SliderInt("Polygon Mode", &(menu->debug.polygon_mode), 0, 3 - 1, current_element_name);
