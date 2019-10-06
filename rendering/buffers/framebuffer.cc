aa8813fb84e51c33444559a6637d9c78d9ba407f
Jacob Panikulam <jpanikul@gmail.com>
2019-10-06 01:18:37 -0400

    Clean up framebuffer abstraction

--- rendering/buffers/framebuffer.cc
+++ rendering/buffers/framebuffer.cc
@@ -30,15 +30,14 @@ void Framebuffer::attach_color_texture(const Texture& texture) {
       GL_COLOR_ATTACHMENT3, GL_COLOR_ATTACHMENT4, GL_COLOR_ATTACHMENT5,
       GL_COLOR_ATTACHMENT6, GL_COLOR_ATTACHMENT7, GL_COLOR_ATTACHMENT8};
 
-  glFramebufferTexture(GL_FRAMEBUFFER, color_attachments[n_color_attachments_],
-                       texture.get_id(), 0);
+  glNamedFramebufferTexture(framebuffer_id_, color_attachments[n_color_attachments_],
+                            texture.get_id(), 0);
   draw_buffers_.push_back(color_attachments[n_color_attachments_]);
 
   ++n_color_attachments_;
 }
+
 void Framebuffer::attach_depth_texture(const Texture& texture) {
-  // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D,
-  // texture.get_id(), 0);
   glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, texture.get_id(), 0);
 }
 
